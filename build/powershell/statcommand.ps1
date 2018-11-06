35c49b7c1e7ad3fb660d1913297deaf5d6f161a1
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2018-11-06 17:00:13 -0500

    Update statcommand.ps1

--- build/powershell/statcommand.ps1
+++ build/powershell/statcommand.ps1
@@ -28,10 +28,10 @@ if($Timeouts -eq "Yes")
  {
   $GetStats = [PSCustomObject]@{}
   if(Test-Path ".\timeout"){Remove-Item ".\timeout" -Force -Recurse}
+  Write-Host "Cleared all bans" -ForegroundColor Green
   Start-Sleep -S 3
   if(Test-Path "stats"){Get-ChildItemContent "stats" | ForEach {$GetStats | Add-Member $_.Name $_.Content}}
   $GetStats
-  Write-Host "Cleared all bans" -ForegroundColor Green
  }
 
 }
