f714df1612a773756f23a000c40e3ee66fb515d8
MaynardMiner <maynardviii@gmail.com>
2018-11-07 00:31:58 -0500

    sexy logo

--- build/powershell/sexyunixlogo.ps1
+++ build/powershell/sexyunixlogo.ps1
@@ -21,7 +21,7 @@ Write-Host "
                                                                       ███████║╚███╔███╔╝██║  ██║██║  ██║██║ ╚═╝ ██║              
                                                                       ╚══════╝ ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝ 
                                                                              Parental Discretion Is Advised 
-                                                                                  v1.7.0 Unix/HiveOS        
+                                                                                  v1.7.1 Unix/HiveOS        
                                                                                                                     
                                                                       GitHub: https://Github.com/MaynardMiner/Swarm      
                                                                                                                     
