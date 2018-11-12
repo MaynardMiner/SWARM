9fa7346846eed1a3a06247507bd4b673b2768a47
MaynardMiner <maynardviii@gmail.com>
2018-11-11 19:18:52 -0500

    typo & fans

--- build/powershell/intensity.ps1
+++ build/powershell/intensity.ps1
@@ -8,12 +8,11 @@ $GetLogMiner = $LogMiner | ConvertFrom-Json
 $ParseLog = ".\logs\$($GetLogMiner.Type).log"
 if(Test-Path $ParseLog)
 {
- $GetInfo = @()
- $GetInfo += Get-Content $ParseLog
+ $GetInfo = Get-Content $ParseLog
  $GetIntensity = $GetInfo | Select-String "intensity"
  $GetDifficulty = $GetInfo | Select-String "difficulty"
  $NotePath = Split-Path $GetLogMiner.Path
- if($GetIntensity){$GetIntensity | Set-Content "$NotePath\$($GetLogMiner.Algo)_Intensity.txt"}
- if($GetDifficulty){$GetInfo | Set-Content "$NotePath\$($GetLogMiner.Algo)_Difficulty.txt"}
+ if($GetIntensity){$GetIntensity | Set-Content "$NotePath\$($GetLogMiner.Algo)_intensity.txt"}
+ if($GetDifficulty){$GetDifficulty | Set-Content "$NotePath\$($GetLogMiner.Algo)_difficulty.txt"}
 }
 }
