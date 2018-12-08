14fe2b78a34c5d643337fbd24207d46c5f61afd5
MaynardMiner <Maynardviii@gmail.com>
2018-12-08 16:51:13 -0500

    add command started

---
+++ build/powershell/add.ps1
@@ -0,0 +1,17 @@
+param(
+        [Parameter(Position=0,Mandatory=$false)]
+        [String]$argument1,
+        [Parameter(Position=1, Mandatory=$false)]
+        [String]$argument2,
+        [Parameter(Position=2,Mandatory=$false)]
+        [String]$argument3,
+        [Parameter(Position=3, Mandatory=$false)]
+        [String]$argument4,
+        [Parameter(Position=4,Mandatory=$false)]
+        [String]$argument5,
+        [Parameter(Position=5, Mandatory=$false)]
+        [String]$argument6,
+        [Parameter(Position=5, Mandatory=$false)]
+        [String]$argument7
+     )
+
