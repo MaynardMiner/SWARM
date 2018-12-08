7437eef8cb7573b18c22b7024e3bd4a9afc4b0e2
MaynardMiner <Maynardviii@gmail.com>
2018-12-08 16:44:55 -0500

    benchmark timeout (downloads)

--- build/powershell/benchmark.ps1
+++ build/powershell/benchmark.ps1
@@ -28,14 +28,40 @@ Switch($command)
   "timeout"
    {
     if(Test-Path ".\timeout"){Remove-Item ".\timeout" -Recurse -Force}
-    $Get += "Removed All Timeouts"
+    $Get += "Removed All Timeouts and Bans"
    }
    "all"
    {
-   if(Test-Path ".\stats\*_hashrate.txt*"){Remove-Item ".\stats\*_hashrate.txt*" -Force}
-   if(Test-Path ".\backup\*_hashrate.txt*"){Remove-Item ".\stats\*_hashrate.txt*" -Force}
-   if(Test-Path ".\timeout\pool_block\pool_block.txt"){Clear-Content ".\timeout\pool_block\pool_block.txt"}
-   if(Test-Path ".\timeout\algo_block\algo_block.txt"){Clear-Content ".\timeout\algo_block\algo_block.txt"}
+   if(Test-Path ".\stats\*_hashrate.txt*")
+   {
+    $Get += "Removed all Hashrates"
+    Remove-Item ".\stats\*_hashrate.txt*" -Force
+   }
+   if(Test-Path ".\backup\*_hashrate.txt*")
+   {
+    $Get += "Removed all backup Hashrates"
+     Remove-Item ".\stats\*_hashrate.txt*" -Force
+   }
+   if(Test-Path ".\timeout\pool_block\pool_block.txt")
+   {
+    $Get += "Removed all pool blocks"
+    Remove-Item ".\timeout\pool_block\pool_block.txt" -Force
+   }
+   if(Test-Path ".\timeout\algo_block\algo_block.txt")
+   {
+    $Get += "Removed all algo blocks"
+    Remove-Item ".\timeout\algo_block\algo_block.txt" -Force
+   }
+   if(Test-Path ".\timeout\download_block\download_block.txt")
+    {
+     $Get += "Removed all download blocks"
+     Clear-Content ".\timeout\download_block\download_block.txt"
+    }
+   if(Test-Path ".\timeout\miner_block\miner_block.txt")
+    {
+     $Get += "Removed all miner blocks" 
+     Clear-Content ".\timeout\miner_block\miner_block.txt"
+    }
    $Get += "Removed All Benchmarks and Bans"
    }
    "miner"
@@ -67,6 +93,14 @@ Switch($command)
      $GetPoolBlock | foreach {if($_.Name -ne $Name){$NewPoolBlock += $_}else{$Get += "Found $($_.Name) in Miner Block file"}}
      if($NewPoolBlock){$NewPoolBlock | ConvertTo-Json | Set-Content ".\timeout\algo_block\algo_block.txt"}
      else{Clear-Content ".\timeout\miner_block\miner_block.txt"}
+    }
+    if(Test-Path ".\timeout\download_block\download_block.txt")
+    {
+     $NewPoolBlock = @()
+     $GetPoolBlock = Get-Content ".\timeout\download_block\download_block.txt" | ConvertFrom-Json
+     $GetPoolBlock | foreach {if($_.Name -ne $Name){$NewPoolBlock += $_}else{$Get += "Found $($_.Name) in Download Block file"}}
+     if($NewPoolBlock){$NewPoolBlock | ConvertTo-Json | Set-Content ".\timeout\algo_block\algo_block.txt"}
+     else{Clear-Content ".\timeout\download_block\download_block.txt"}
     }
     $Get += "Removed all $Name stats and bans."
     }
@@ -103,5 +137,5 @@ Switch($command)
     $Get += "No Command Given"
   }
 }
-
+$Get += "Effects will taked place after next miner benchmark/interval period."
 $Get | Out-File ".\build\txt\get.txt"
