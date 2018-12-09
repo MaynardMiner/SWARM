48d4dd47a5332c7949024ffcfa38a96b14d6f33b
MaynardMiner <Maynardviii@gmail.com>
2018-12-09 18:39:53 -0500

    all yiimp wallets

---
+++ wallet/pools/phiphipool.ps1
@@ -0,0 +1,29 @@
+. .\build\powershell\childitems.ps1
+. .\build\powershell\statcommand.ps1
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+$Getkeys = if(Test-Path ".\wallet\keys"){Get-ChildItemContent ".\wallet\keys" | % {$_.Content | Add-Member @{Name = $_.Content.Wallet} -PassThru}}
+$AltWallets = ("AltWallet1","AltWallet2","AltWallet3")
+$AltPool = $false
+if($AltPool = $true){$Wallets = $AltWallets}else{$Wallets = ("Wallet1","Wallet2","Wallet3")}
+$PoolQuery = "http://phi-phi-pool.com/api/wallet?address="
+
+$Getkeys | %{if($Wallets -match $_.Name)
+  {
+    try
+    {
+     $Response = Invoke-RestMethod "$PoolQuery$($_.address)" -UseBasicParsing -TimeoutSec 10 -ErrorAction Stop
+    }
+    Catch{Write-Warning "failed to contact $Name For Wallet Info"; $Response = $Null}
+    $_.Response = $Response
+   }
+  }
+
+$Getkeys | % {
+    if($_.Response)
+     {
+      Set-WStat -Name "$($Name)_$($_.Address)" -Symbol $_.symbol -address $_.address -balance $_.response.balance -unpaid $_.response.unpaid
+    }
+   }
+   
---
+++ wallet/pools/starpool.ps1
@@ -0,0 +1,30 @@
+. .\build\powershell\childitems.ps1
+. .\build\powershell\statcommand.ps1
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+$Getkeys = if(Test-Path ".\wallet\keys"){Get-ChildItemContent ".\wallet\keys" | % {$_.Content | Add-Member @{Name = $_.Content.Wallet} -PassThru}}
+$AltWallets = ("AltWallet1","AltWallet2","AltWallet3")
+$AltPool = $false
+$Getkeys | %{if($AltWallets -eq $_.Name){$AltPool = $true}}
+if($AltPool = $true){$Wallets = $AltWallets}else{$Wallets = ("Wallet1","Wallet2","Wallet3")}
+$PoolQuery = "https://www.starpool.biz/api/wallet?address="
+
+$Getkeys | %{if($Wallets -match $_.Name)
+  {
+    try
+    {
+     $Response = Invoke-RestMethod "$PoolQuery$($_.address)" -UseBasicParsing -TimeoutSec 10 -ErrorAction Stop
+    }
+    Catch{Write-Warning "failed to contact $Name For Wallet Info"; $Response = $Null}
+    $_.Response = $Response
+   }
+  }
+
+$Getkeys | % {
+ if($_.Response)
+  {
+    Set-WStat -Name "$($Name)_$($_.Address)" -Symbol $_.symbol -address $_.address -balance $_.response.balance -unpaid $_.response.unpaid
+  }
+}
+
