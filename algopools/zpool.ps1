ff3d95fcd0f22ad4ee4d997c649c8e44ba520fe7
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2018-12-18 19:12:05 -0500

    zpool na

--- algopools/zpool.ps1
+++ algopools/zpool.ps1
@@ -27,7 +27,7 @@ $Zpool_Request | Get-Member -MemberType NoteProperty -ErrorAction Ignore | Selec
     $Zpool_Algorithm = Get-Algorithm $Zpool_Request.$_.name
     Switch($Location)
     {
-     "US"{$Zpool_Host = "$ZPool_Algorithm.eu.mine.zpool.ca"}
+     "US"{$Zpool_Host = "$ZPool_Algorithm.na.mine.zpool.ca"}
      "Europe"{$Zpool_Host = "$ZPool_Algorithm.eu.mine.zpool.ca"}
      "Asia"{$Zpool_Host = "$ZPool_Algorithm.sea.mine.zpool.ca"}
     }
