ebd9068e310471390781fcb1c4ecbb159d3503c7
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2019-01-17 20:12:22 -0500

    rebuilding buggy version command

--- build/powershell/version.ps1
+++ build/powershell/version.ps1
@@ -25,227 +25,138 @@ param(
     [parameter(Mandatory=$false)]
     [String]$Platform
 )
+
+## Set to SWARM dir
 Set-Location (Split-Path (Split-Path (Split-Path $script:MyInvocation.MyCommand.Path)))
+$Message = @()
 
-if($Platform -ne "windows")
-{
-if($Command -eq "!"){Write-Host "No Command Given. Try version query"}
-else{$CommandQuery = $Command -replace("!","")
-Write-Host "Executing Command: $CommandQuery"
-Write-Host "                   " }
-$CudaVersion = Get-Content ".\build\txt\cuda.txt"
-if($CudaVersion -eq "9.2"){$updatecheck = ".\config\update\nvidia9.2-linux.json"; $miner_update_nvidia = Get-Content ".\config\update\nvidia9.2-linux.json" | ConvertFrom-Json}
-if($CudaVersion -eq "10"){$updatecheck = ".\config\update\nvidia10-linux.json"; $miner_update_nvidia = Get-Content ".\config\update\nvidia10-linux.json" | ConvertFrom-Json}
-$miner_update_amd = Get-Content ".\config\update\amd-linux.json" | ConvertFrom-Json
-$miner_update_cpu = Get-Content ".\config\update\cpu-linux.json" | ConvertFrom-Json
+if($Command -eq "!"){$Message += "No Command Given. Try version query";}
+else{$CommandQuery = $Command -replace("!","")}
 
-$nvidia = [PSCustomObject]@{}
-$amd = [PSCustomObject]@{}
-$cpu = [PSCustomObject]@{}
-
-$miner_update_nvidia | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | foreach {$nvidia | Add-Member $miner_update_nvidia.$_.Name $miner_update_nvidia.$_}
-$miner_update_amd | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | foreach {$amd | Add-Member $miner_update_amd.$_.Name $miner_update_amd.$_}
-$miner_update_cpu | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | foreach {$cpu | Add-Member $miner_update_cpu.$_.Name $miner_update_cpu.$_}
-
-$nvidiatable = @()
-$amdtable = @()
-$cputable = @()
-
-$nvidia | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | foreach {
-    $nvidiatable += [PSCustomObject]@{
-     Name = $nvidia.$_.Name
-     Type = $nvidia.$_.Type
-     MinerName = $nvidia.$_.MinerName
-     Version = $nvidia.$_.version
-    }
-}
-
-$amd | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | foreach {
-    $amdtable += [PSCustomObject]@{
-     Name = $amd.$_.Name
-     Type = $amd.$_.Type
-     MinerName = $amd.$_.MinerName
-     Version = $amd.$_.version
-    }
-}
-
-$cpu | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | foreach {
-    $cputable += [PSCustomObject]@{
-     Name = $cpu.$_.Name
-     Type = $cpu.$_.Type
-     MinerName = $cpu.$_.MinerName
-     Version = $cpu.$_.version
-    }
-}
-
-
-
-if($Command -eq "!query")
+if($CommandQuery)
  {
-    $nvidiatable | Sort-Object -Property Type,Name | Format-Table (
-        @{Label = "Name"; Expression={$($_.Name)}},
-        @{Label = "Type"; Expression={$($_.Type)}},
-        @{Label = "Executable"; Expression={$($_.MinerName)}},
-        @{Label = "Version"; Expression={$($_.Version)}}
-    ) | Out-Host
-    $amdtable | Sort-Object -Property Type,Name | Format-Table (
-        @{Label = "Name"; Expression={$($_.Name)}},
-        @{Label = "Type"; Expression={$($_.Type)}},
-        @{Label = "Executable"; Expression={$($_.MinerName)}},
-        @{Label = "Version"; Expression={$($_.Version)}}
-    ) | Out-Host
-    $cputable | Sort-Object -Property Type,Name | Format-Table (
-        @{Label = "Name"; Expression={$($_.Name)}},
-        @{Label = "Type"; Expression={$($_.Type)}},
-        @{Label = "Executable"; Expression={$($_.MinerName)}},
-        @{Label = "Version"; Expression={$($_.Version)}}
-    ) | Out-Host
-}
-
-if($Command -eq "!update")
- {
-    $Name = $Name -replace ("!","")
-    Write-Host "Stopping Miner & Waiting 5 Seconds"
-    miner stop
-    Start-Sleep -S 5
-    $newEXE = $EXE -replace("!","")
-    $newVersion = $Version -replace("!","")
-    $newURI = $uri -replace("!","")
-    $nvidiaupdate = $false
-    $amdupdate = $false
-    $cpuupdate = $false
-
-    $nvidia | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | foreach {
-     if($Name -eq $nvidia.$_.name)
-      {
-       $nvidia.$_.minername = $newEXE 
-       $nvidia.$_.version = $newversion
-       $nvidia.$_.uri = $newuri 
-       Write-Host "$Name new executable is $newEXE"
-       Write-Host "$Name new uri is $newuri"
-       Write-Host "$Name new version is $newversion"
-       if(Test-Path ".\bin\*$Name*"){Remove-Item ".\bin\*$Name*" -Recurse -Force}
-       $nvidiaupdate = $true
-       if(Test-Path ".\bin\*$Name*"){Remove-Item ".\bin\*$Name*" -Recurse -Force};
-       $nvidia | ConvertTo-Json | Out-File $updatecheck
-      }
-    }
-
-    $amd | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | foreach {
-     if($Name -eq $amd.$_.name)
-      {
-       $amd.$_.minername = $newEXE
-       $amd.$_.version = $newVersion
-       $amd.$_.uri = $newuri
-       Write-Host "$Name new executable is $newexe"
-       Write-Host "$Name new uri is $newuri"
-       Write-Host "$Name new version is $newversion"
-       if(Test-Path ".\bin\*$Name*"){Remove-Item ".\bin\*$Name*" -Recurse -Force}
-       $amd | ConvertTo-Json | Out-File ".\config\update\amd-linux.json"
-      }
-    }
-
-    $cpu | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | foreach {
-     if($Name -eq $cpu.$_.name)
-      {
-       $cpu.$_.minername = $newEXE
-       $cpu.$_.version = $newVersion
-       $cpu.$_.uri = $newuri
-       Write-Host "$Name new executable is $newexe"
-       Write-Host "$Name new uri is $newuri"
-       Write-Host "$Name new version is $newversion"
-       if(Test-Path ".\bin\*$Name*"){Remove-Item ".\bin\*$cpu*" -Recurse -Force}
-       $cpu | ConvertTo-Json | Out-File ".\config\update\cpu-linux.json"
-      }
-    }
-
-    Write-Host "Miner Was Updated" -ForegroundColor Green
-    Write-Host "Restarting Miner With New Settings"
-    miner start
-}
-}
-else {
-    if($Command -eq $null){"No Command Given. Try version query" | Out-File ".\build\txt\version.txt"}
-    "Executing Command: $Command" | Out-File ".\build\txt\version.txt"
-    "                   " | Out-File ".\build\txt\version.txt" -Append
-    $miner_update_nvidia = Get-Content ".\config\update\nvidia-win.json" | ConvertFrom-Json
-    $miner_update_cpu = Get-Content ".\config\update\cpu-win.json" | ConvertFrom-Json
-    $cpu = [PSCustomObject]@{}
-    $nvidia = [PSCustomObject]@{}
-    $miner_update_nvidia | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | foreach {$nvidia | Add-Member $miner_update_nvidia.$_.Name $miner_update_nvidia.$_}
-    $miner_update_cpu | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | foreach {$cpu | Add-Member $miner_update_cpu.$_.Name $miner_update_cpu.$_}
-    $nvidiatable = @()
-    $cputable = @()
-
-    $nvidia | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | foreach {$nvidiatable += [PSCustomObject]@{ Name = $nvidia.$_.Name; Type = $nvidia.$_.Type; MinerName = $nvidia.$_.MinerName; Version = $nvidia.$_.version;}}
- 
-    $cpu | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | foreach {
-        $cputable += [PSCustomObject]@{
-         Name = $cpu.$_.Name
-         Type = $cpu.$_.Type
-         MinerName = $cpu.$_.MinerName
-         Version = $cpu.$_.version
-        }
-    }
-    
-    if($Command -eq "query")
+  $Message += "Command is $CommandQuery"
+  $GetCuda = ".\build\txt\cuda.txt"
+  if(test-path $GetCuda){$CudaVersion = Get-Content $GetCuda}
+  else{$Message += "Warning: Unable to detect cuda version."}
+  Switch($Platform)
+  {
+   "windows"
     {
-       $nvidiatable | Sort-Object -Property Type,Name | Format-Table (
-           @{Label = "Name"; Expression={$($_.Name)}},
-           @{Label = "Type"; Expression={$($_.Type)}},
-           @{Label = "Executable"; Expression={$($_.MinerName)}},
-           @{Label = "Version"; Expression={$($_.Version)}}
-       ) | Out-File ".\build\txt\version.txt" -Append
-       $cputable | Sort-Object -Property Type,Name | Format-Table (
-           @{Label = "Name"; Expression={$($_.Name)}},
-           @{Label = "Type"; Expression={$($_.Type)}},
-           @{Label = "Executable"; Expression={$($_.MinerName)}},
-           @{Label = "Version"; Expression={$($_.Version)}}
-       ) | Out-File ".\build\txt\version.txt" -Append
-   }
-
-   if($Command -eq "update")
+     $miner_update_nvidia = Get-Content ".\config\update\nvidia-win.json" | ConvertFrom-Json
+     $miner_update_amd = Get-Content ".\config\update\amd-win.json" | ConvertFrom-Json
+     $miner_update_cpu = Get-Content ".\config\update\cpu-win.json" | ConvertFrom-Json
+    } 
+   default
    {
-      "Stopping Miner & Waiting 5 Seconds" | Out-File ".\build\txt\version.txt" -Append
-      $ID = Get-Content ".\build\pid\miner_pid.txt"
-      Stop-Process -Id $ID
-      Start-Sleep -S 5
-      $newEXE = $EXE 
-      $newVersion = $Version 
-      $newURI = $uri 
-      $nvidiaupdate = $false
-      $amdupdate = $false
-      $cpuupdate = $false
-  
-      $nvidia | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | foreach {
-         if($Name -eq $nvidia.$_.name){
-         $nvidia.$_.minername = $newEXE; 
-         $nvidia.$_.version = $newversion;
-         $nvidia.$_.uri = $newuri ;
-         "$Name new executable is $newEXE" | Out-File ".\build\txt\version.txt" -Append;
-         "$Name new uri is $newuri" | Out-File ".\build\txt\version.txt" -Append;
-         "$Name new version is $newversion" | Out-File ".\build\txt\version.txt" -Append;
-         if(Test-Path ".\bin\*$Name*"){Remove-Item ".\bin\*$Name*" -Recurse -Force};
-         $nvidia | ConvertTo-Json | Out-File ".\config\update\nvidia-win.json"
-        }
-      }
-    
-      $cpu | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | foreach {
-       if($Name -eq $cpu.$_.name)
-        {
-         $cpu.$_.minername = $newEXE
-         $cpu.$_.version = $newVersion
-         $cpu.$_.uri = $newuri
-         "$Name new executable is $newexe" | Out-File ".\build\txt\version.txt" -Append
-         "$Name new uri is $newuri" | Out-File ".\build\txt\version.txt" -Append
-         "$Name new version is $newversion" | Out-File ".\build\txt\version.txt" -Append
-         if(Test-Path ".\bin\*$Name*"){Remove-Item ".\bin\*$cpu*" -Recurse -Force}
-         $cpu | ConvertTo-Json | Out-File ".\config\update\cpu-win.json"
-        }
-      }
-  
-     "Miner Was Updated" | Out-File ".\build\txt\version.txt" -Append
-      "Restarting Miner With New Settings" | Out-File ".\build\txt\version.txt" -Append
+    Switch($CudaVersion)
+    {
+    "9.2"
+     {
+      $updatecheck = ".\config\update\nvidia9.2-linux.json"
+      if(Test-Path $updatecheck){$miner_update_nvidia = Get-Content $updatecheck | ConvertFrom-Json}
+      else{$Message += "Warning: Cuda 9.2 update file not found"}
+     }
+    "10"
+     {
+      $updatecheck = ".\config\update\nvidia10-linux.json"
+      if(Test-Path $updatecheck){$miner_update_nvidia = Get-Content $updatecheck | ConvertFrom-Json}
+      else{$Message += "Warning: Cuda 10 update file not found"}
+     }
     }
-  
+    $miner_update_amd = Get-Content ".\config\update\amd-linux.json" | ConvertFrom-Json
+    $miner_update_cpu = Get-Content ".\config\update\cpu-linux.json" | ConvertFrom-Json  
+  }
+  }
+
+$MinerSearch = @()
+$MinerSearch += $miner_update_nvidia
+$MinerSearch += $miner_update_amd
+$MinerSearch += $miner_update_cpu
+
+ switch($CommandQuery)
+  {
+   "Update"
+    {
+     $Message += "Executing Command: $CommandQuery"
+     $Failed = $false
+     $Found = $false
+
+     $MinerSearch | %{
+     $MinerType = $_
+     $MinerType.PSObject.Properties.Name | %{
+     if($MinerType.$_.name -eq $Name)
+      {
+       $Found = $true
+       $UpdateFile = $MinerType.Name
+       if($EXE){$MinerType.$_.minername = $EXE}
+       else{$Message += "No exe supplied. Please run again."; $failed = $true}
+       if($Version){$MinerType.$_.version = $Version}
+       else{$Message += "No version supplied. Please run again."; $failed = $true}
+       if($Uri){$MinerType.$_.uri = $Uri}
+       else{$Message += "No Uri Supplied. Please run again."; $failed = $true}
+      }
+     }
+    } 
+    
+    if($Found -eq $true -and $Failed -eq $false)
+    {
+     $Message += "Stopping Miner & Waiting 5 Seconds"
+     switch ($Platform)
+      {
+       "windows"
+        {
+         $ID = Get-Content ".\build\pid\miner_pid.txt"
+         Stop-Process -Id $ID
+         Start-Sleep -S 5
+        }
+        default
+        {
+         screen -S miner -X quit
+         Start-Sleep -S 5
+        }
+      }
+     $Updated = $MinerType | Where Name -eq $UpdateFile
+     if(Test-Path $Updated.$Name.path1){Remove-Item (Split-Path $Updated.$Name.path1) -Recurse -Force}
+     if(Test-Path $Updated.$Name.path2){Remove-Item (Split-Path $Updated.$Name.path2) -Recurse -Force}
+     if(Test-Path $Updated.$Name.path3){Remove-Item (Split-Path $Updated.$Name.path3) -Recurse -Force}
+     $Updated | Set-Content ".\config\update\$UpdateFile.json"
+     $message += "Miner New executable is $EXE"
+     $message += "Miner New version is $Version"
+     $message += "Miner New uri is $URI"
+     $message += "Miner Was Updated"
+     if($Platform -ne "windows")
+     {
+     if(-not (test-path "/hive/miners/custom"))
+      {
+       $Message += "Restarting Swarm in miner screen"
+       Write-Host $Message
+       $SWARM= Get-Content "swarm" -replace "screen -S miner -m "
+       Start-Sleep -S 5
+       screen -S miner -d -m $SWARM
+      }
+     else
+      {
+       $Message += "Restarting Swarm"
+       miner start
+      }
+     }
+    }
+   else{$message += "Miner update process failed. Exiting"}
+  }
+
+ "query"
+  {
+   $MinerTables = @()
+    $MinerSearch | %{
+     $MinerTable = $_
+     $MinerTable = $MinerTable.PSObject.Properties.Name | %{if($_ -ne "name"){$MinerTable.$_}}
+     $MinerTable |  Sort-Object -Property Type,Name | Format-Table (@{Label = "Name"; Expression={$($_.Name)}},@{Label = "Type"; Expression={$($_.Type)}},@{Label = "Executable"; Expression={$($_.MinerName)}},@{Label = "Version"; Expression={$($_.Version)}}) | Out-Host
+    }
+   }
+  }
  }
+ 
+ $Message | Out-File ".\build\txt\version.txt"
+ Write-Host "$Message"
+
