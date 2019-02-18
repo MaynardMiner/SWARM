db27686f9d9dc78ec70e2fa6c89cbe156cab7f06
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2019-02-17 23:25:19 -0500

    better stream output windows

--- build/powershell/launchcode.ps1
+++ build/powershell/launchcode.ps1
@@ -169,7 +169,7 @@ function Start-LaunchCode {
                 $miner_bat = Join-Path $WorkingDirectory "swarm-start.bat"
                 $minerbat | Set-Content $miner_bat
             }
-            $script += "Invoke-Expression `'.\$($MinerCurrent.MinerName) $($MinerArguments) | %{`$Output = `$_ -replace `"\\[\d+(;\d+)?m`"; `$OutPut | Out-File -FIlePath ""$Logs"" -Append; `$_ | Out-Host}`'"
+            $script += "Invoke-Expression `'.\$($MinerCurrent.MinerName) $($MinerArguments) *>&1 | %{`$Output = `$_ -replace `"\\[\d+(;\d+)?m`"; `$OutPut | Out-File -FIlePath ""$Logs"" -Append; `$_ | Out-Host}`'"
             $script | out-file "$WorkingDirectory\swarm-start.ps1"
             Start-Sleep -S .5
 
