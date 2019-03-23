726c3ff25daf159baca582ddc6119a45825821b4
MaynardMiner <MaynardVIII@gmail.com>
2019-03-22 23:03:58 -0400

    2.1.0 Alpha

--- build/powershell/remoteupdate.ps1
+++ build/powershell/remoteupdate.ps1
@@ -44,6 +44,7 @@ function start-update {
         $PreviousVersions += "SWARM.2.0.6"
         $PreviousVersions += "SWARM.2.0.7"
         $PreviousVersions += "SWARM.2.0.8"
+        $PreviousVersions += "SWARM.2.0.9"
 
         Write-Host "User Specfied Updates: Searching For Previous Version" -ForegroundColor Yellow
         Write-Host "Check $Location For any Previous Versions"
