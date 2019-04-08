fc840591726488abb28995781a968963876029aa
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2019-04-08 18:58:39 -0400

    bump version 2.1.4

--- build/powershell/remoteupdate.ps1
+++ build/powershell/remoteupdate.ps1
@@ -48,6 +48,7 @@ function start-update {
         $PreviousVersions += "SWARM.2.1.0"
         $PreviousVersions += "SWARM.2.1.1"
         $PreviousVersions += "SWARM.2.1.2"
+        $PreviousVersions += "SWARM.2.1.3"
 
         Write-Host "User Specfied Updates: Searching For Previous Version" -ForegroundColor Yellow
         Write-Host "Check $Location For any Previous Versions"
