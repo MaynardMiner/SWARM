7522cac4c9211a76bffadefaee6ca606797ff8f9
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2019-04-21 19:08:09 -0400

    update ehssand_amd

--- miners/optional_and_old/ehssand_amd-1.ps1
+++ miners/optional_and_old/ehssand_amd-1.ps1
@@ -1,82 +1,86 @@
-##Miner Path Information
-if ($amd.ehssand_amd.path1) { $Path = "$($amd.ehssand_amd.path1)" }
-else { $Path = "None" }
-if ($amd.ehssand_amd.uri) { $Uri = "$($amd.ehssand_amd.uri)" }
-else { $Uri = "None" }
-if ($amd.ehssand_amd.minername) { $MinerName = "$($amd.ehssand_amd.minername)" }
-else { $MinerName = "None" }
-if ($Platform -eq "linux") { $Build = "Tar" }
-elseif ($Platform -eq "windows") { $Build = "Zip" }
+$AMDTypes | ForEach-Object {
+    
+    $ConfigType = $_; $Num = $ConfigType -replace "AMD", ""
 
-$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+    if ($amd.ehssand_amd.$ConfigType) { $Path = "$($amd.ehssand_amd.$ConfigType)" }
+    else { $Path = "None" }
+    if ($amd.ehssand_amd.uri) { $Uri = "$($amd.ehssand_amd.uri)" }
+    else { $Uri = "None" }
+    if ($amd.ehssand_amd.minername) { $MinerName = "$($amd.ehssand_amd.minername)" }
+    else { $MinerName = "None" }
+    if ($Platform -eq "linux") { $Build = "Tar" }
+    elseif ($Platform -eq "windows") { $Build = "Zip" }
 
-$ConfigType = "AMD1"
-$User = "User1"
+    $User = "User$Num"; $Pass = "Pass$Num"; $Name = "ehssand_amd-$Num"; $Port = "3400$Num"
 
-##Log Directory
-$Log = Join-Path $dir "logs\$ConfigType.log"
+    Switch ($Num) {
+        1 { $Get_Devices = $AMDDevices1 }
+    }
+    ##Log Directory
+    $Log = Join-Path $dir "logs\$ConfigType.log"
 
-##Parse -GPUDevices
-if ($AMDDevices1 -ne "none") { $Devices = $AMDDevices1 }
-else { $Devices = $Get_Devices }
+    ##Parse -GPUDevices
+    if ($Get_Devices -ne "none") { $Devices = $Get_Devices }
+    else { $Devices = $Get_Devices }
 
-##Get Configuration File
-$GetConfig = "$dir\config\miners\ehssand_amd.json"
-try { $Config = Get-Content $GetConfig | ConvertFrom-Json }
-catch { Write-Warning "Warning: No config found at $GetConfig" }
+    ##Get Configuration File
+    $GetConfig = "$dir\config\miners\ehssand_amd.json"
+    try { $Config = Get-Content $GetConfig | ConvertFrom-Json }
+    catch { Write-Warning "Warning: No config found at $GetConfig" }
 
-##Export would be /path/to/[SWARMVERSION]/build/export && Bleeding Edge Check##
-$ExportDir = Join-Path $dir "build\export"
+    ##Export would be /path/to/[SWARMVERSION]/build/export && Bleeding Edge Check##
+    $ExportDir = Join-Path $dir "build\export"
 
-##Prestart actions before miner launch
-$BE = "/usr/lib/x86_64-linux-gnu/libcurl-compat.so.3.0.0"
-$Prestart = @()
-if (Test-Path $BE) { $Prestart += "export LD_PRELOAD=libcurl-compat.so.3.0.0" }
-$PreStart += "export LD_LIBRARY_PATH=$ExportDir"
-$Config.$ConfigType.prestart | ForEach-Object { $Prestart += "$($_)" }
+    ##Prestart actions before miner launch
+    $BE = "/usr/lib/x86_64-linux-gnu/libcurl-compat.so.3.0.0"
+    $Prestart = @()
+    if (Test-Path $BE) { $Prestart += "export LD_PRELOAD=libcurl-compat.so.3.0.0" }
+    $PreStart += "export LD_LIBRARY_PATH=$ExportDir"
+    $Config.$ConfigType.prestart | ForEach-Object { $Prestart += "$($_)" }
 
-if ($Coins -eq $true) { $Pools = $CoinPools }else { $Pools = $AlgoPools }
-$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+    if ($Coins -eq $true) { $Pools = $CoinPools }else { $Pools = $AlgoPools }
 
-##Build Miner Settings
-$Config.$ConfigType.commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
-    $MinerAlgo = $_
-    $Stat = Get-Stat -Name "$($Name)_$($MinerAlgo)_hashrate"
-    $Pools | Where-Object Algorithm -eq $MinerAlgo | ForEach-Object {
-        if ($Algorithm -eq "$($_.Algorithm)" -and $Bad_Miners.$($_.Algorithm) -notcontains $Name) {
-            if ($Config.$ConfigType.difficulty.$($_.Algorithm)) { $Diff = ",d=$($Config.$ConfigType.difficulty.$($_.Algorithm))" }else { $Diff = "" }
-            [PSCustomObject]@{
-                Coin       = $Coins
-                Delay      = $Config.$ConfigType.delay
-                Fees       = $Config.$ConfigType.fee.$($_.Algorithm)
-                Symbol     = "$($_.Symbol)"
-                MinerName  = $MinerName
-                Prestart   = $PreStart
-                Type       = $ConfigType
-                Path       = $Path
-                Devices    = $Devices
-                DeviceCall = "sgminer-gm"
-                Arguments  = "--gpu-platform $AMDPlatform --api-listen --api-port 4008 -k $($Config.$ConfigType.naming.$($_.Algorithm)) -o stratum+tcp://$($_.Host):$($_.Port) -u $($_.User1) -p $($_.Pass1)$($Diff) -T $($Config.$ConfigType.commands.$($_.Algorithm))"
-                HashRates  = [PSCustomObject]@{$($_.Algorithm) = $Stat.Day }
-                Quote      = if ($Stat.Day) { $Stat.Day * ($_.Price) }else { 0 }
-                PowerX     = [PSCustomObject]@{$($_.Algorithm) = if ($Watts.$($_.Algorithm)."$($ConfigType)_Watts") { $Watts.$($_.Algorithm)."$($ConfigType)_Watts" }elseif ($Watts.default."$($ConfigType)_Watts") { $Watts.default."$($ConfigType)_Watts" }else { 0 } }
-                ocdpm      = if ($Config.$ConfigType.oc.$($_.Algorithm).dpm) { $Config.$ConfigType.oc.$($_.Algorithm).dpm }else { $OC."default_$($ConfigType)".dpm }
-                ocv        = if ($Config.$ConfigType.oc.$($_.Algorithm).v) { $Config.$ConfigType.oc.$($_.Algorithm).v }else { $OC."default_$($ConfigType)".v }
-                occore     = if ($Config.$ConfigType.oc.$($_.Algorithm).core) { $Config.$ConfigType.oc.$($_.Algorithm).core }else { $OC."default_$($ConfigType)".core }
-                ocmem      = if ($Config.$ConfigType.oc.$($_.Algorithm).mem) { $Config.$ConfigType.oc.$($_.Algorithm).mem }else { $OC."default_$($ConfigType)".memory }
-                ocmdpm     = if ($Config.$ConfigType.oc.$($_.Algorithm).mdpm) { $Config.$ConfigType.oc.$($_.Algorithm).mdpm }else { $OC."default_$($ConfigType)".mdpm }
-                ocfans     = if ($Config.$ConfigType.oc.$($_.Algorithm).fans) { $Config.$ConfigType.oc.$($_.Algorithm).fans }else { $OC."default_$($ConfigType)".fans }
-                MinerPool  = "$($_.Name)"
-                FullName   = "$($_.Mining)"
-                Port       = 4008
-                API        = "sgminer-gm"
-                Wallet     = "$($_.$User)"
-                URI        = $Uri
-                Server     = "localhost"
-                BUILD      = $Build
-                Algo       = "$($_.Algorithm)"
-                Log        = $Log 
-            }            
+    ##Build Miner Settings
+    $Config.$ConfigType.commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
+        $MinerAlgo = $_
+        $Stat = Get-Stat -Name "$($Name)_$($MinerAlgo)_hashrate"
+        $Pools | Where-Object Algorithm -eq $MinerAlgo | ForEach-Object {
+            if ($Algorithm -eq "$($_.Algorithm)" -and $Bad_Miners.$($_.Algorithm) -notcontains $Name) {
+                if ($Config.$ConfigType.difficulty.$($_.Algorithm)) { $Diff = ",d=$($Config.$ConfigType.difficulty.$($_.Algorithm))" }else { $Diff = "" }
+                [PSCustomObject]@{
+                    MName      = $Name
+                    Coin       = $Coins
+                    Delay      = $Config.$ConfigType.delay
+                    Fees       = $Config.$ConfigType.fee.$($_.Algorithm)
+                    Symbol     = "$($_.Symbol)"
+                    MinerName  = $MinerName
+                    Prestart   = $PreStart
+                    Type       = $ConfigType
+                    Path       = $Path
+                    Devices    = $Devices
+                    DeviceCall = "sgminer-gm"
+                    Arguments  = "--gpu-platform $AMDPlatform --api-listen --api-port $Port -k $($Config.$ConfigType.naming.$($_.Algorithm)) -o stratum+tcp://$($_.Host):$($_.Port) -u $($_.$User) -p $($_.$Pass)$($Diff) -T $($Config.$ConfigType.commands.$($_.Algorithm))"
+                    HashRates  = [PSCustomObject]@{$($_.Algorithm) = $Stat.Day }
+                    Quote      = if ($Stat.Day) { $Stat.Day * ($_.Price) }else { 0 }
+                    PowerX     = [PSCustomObject]@{$($_.Algorithm) = if ($Watts.$($_.Algorithm)."$($ConfigType)_Watts") { $Watts.$($_.Algorithm)."$($ConfigType)_Watts" }elseif ($Watts.default."$($ConfigType)_Watts") { $Watts.default."$($ConfigType)_Watts" }else { 0 } }
+                    ocdpm      = if ($Config.$ConfigType.oc.$($_.Algorithm).dpm) { $Config.$ConfigType.oc.$($_.Algorithm).dpm }else { $OC."default_$($ConfigType)".dpm }
+                    ocv        = if ($Config.$ConfigType.oc.$($_.Algorithm).v) { $Config.$ConfigType.oc.$($_.Algorithm).v }else { $OC."default_$($ConfigType)".v }
+                    occore     = if ($Config.$ConfigType.oc.$($_.Algorithm).core) { $Config.$ConfigType.oc.$($_.Algorithm).core }else { $OC."default_$($ConfigType)".core }
+                    ocmem      = if ($Config.$ConfigType.oc.$($_.Algorithm).mem) { $Config.$ConfigType.oc.$($_.Algorithm).mem }else { $OC."default_$($ConfigType)".memory }
+                    ocmdpm     = if ($Config.$ConfigType.oc.$($_.Algorithm).mdpm) { $Config.$ConfigType.oc.$($_.Algorithm).mdpm }else { $OC."default_$($ConfigType)".mdpm }
+                    ocfans     = if ($Config.$ConfigType.oc.$($_.Algorithm).fans) { $Config.$ConfigType.oc.$($_.Algorithm).fans }else { $OC."default_$($ConfigType)".fans }
+                    MinerPool  = "$($_.Name)"
+                    FullName   = "$($_.Mining)"
+                    Port       = $Port
+                    API        = "sgminer-gm"
+                    Wallet     = "$($_.$User)"
+                    URI        = $Uri
+                    Server     = "localhost"
+                    BUILD      = $Build
+                    Algo       = "$($_.Algorithm)"
+                    Log        = $Log 
+                }            
+            }
         }
     }
 }
