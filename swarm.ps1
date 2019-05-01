af880434e4313c4583a13f77a0c36ecd2698998b
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2019-05-01 19:00:03 -0400

    Fix Language Translation For RAM

--- swarm.ps1
+++ swarm.ps1
@@ -641,9 +641,7 @@ Making a terminal on desktop. This can be used for commands.
     }
 
     ## Fetch Ram Size, Write It To File (For Commands)
-    $TotalMemory = (systeminfo | Select-String 'Total Physical Memory:').ToString().Split(':')[1].Trim()
-    $TotalMemory = $TotalMemory -replace (",", "")
-    $TotalMemory = $TotalMemory -replace ("MB", "")
+    $TotalMemory = [math]::Round((Get-CimInstance -ClassName CIM_ComputerSystem).TotalPhysicalMemory / 1GB, 2) 
     $TotalMemory | Set-Content ".\build\txt\ram.txt"
 
     ## GPU Bus Hash Table
