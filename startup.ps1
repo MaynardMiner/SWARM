85b0fd9223118446d73075ddf2eb26ca1be4e475
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2019-05-02 20:34:15 -0400

    Startup

--- startup.ps1
+++ startup.ps1
@@ -1,7 +1,7 @@
-$dir = (Split-Path $script:MyInvocation.MyCommand.Path)
+$dir = Split-Path $script:MyInvocation.MyCommand.Path
 $ParseArgs = Get-Content ".\SWARM.bat"
 $Arguments = $ParseArgs | Select-String "startup.ps1"
 $Arguments -match "startup.ps1 (?<content>.*)`"" | Out-Null
 $Arguments = $matches['content']
 
-Start-Process "CMD" -ArgumentList "/C powershell -version 5.0 -noexit -executionpolicy Bypass -windowstyle maximized -command `"$dir\swarm.ps1 $Arguments`"" -Verb RunAs
+Start-Process "CMD" -ArgumentList "/C powershell -Version 5.0 -noexit -executionpolicy Bypass -windowstyle maximized -command `'$dir\swarm.ps1 $Arguments`'" -Verb RunAs
