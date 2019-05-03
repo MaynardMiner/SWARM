885bffd7d158722bafca5757644eeefc3faa4f8e
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2019-05-03 11:04:22 -0400

    Win 7 auto gpu detect

--- build/powershell/bus.ps1
+++ build/powershell/bus.ps1
@@ -54,8 +54,9 @@ Function Resolve-PCIBusInfo {
     
 Function Get-BusFunctionID {
     #gwmi -query "SELECT * FROM Win32_PnPEntity"
-    $Devices = Get-CimInstance -namespace root\cimv2 -class Win32_PnPEntity | Where PNPClass -Eq "Display"
-
+    $Services = @("nvlddmkm","amdkmdap","igfx","BasicDisplay")
+    Get-CimInstance -namespace root\cimv2 -class Win32_PnPEntity | where Service -in $Services
+    
     for ($i = 0; $i -lt $Devices.Count; $i++) {
         $deviceId = $Devices[$i].PNPDeviceID
         $locationInfo = (get-itemproperty -path "HKLM:\SYSTEM\CurrentControlSet\Enum\$deviceID" -name locationinformation).locationINformation
