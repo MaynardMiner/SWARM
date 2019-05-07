af2f27bb722e096eedf4ec54b8b45bb61be9b587
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2019-05-07 18:54:38 -0400

    .1 dev increase

--- build/powershell/sexyunixlogo.ps1
+++ build/powershell/sexyunixlogo.ps1
@@ -53,7 +53,7 @@ Write-Log "
 					                                    Hybrid Auto-Profit Switching Miner
 						       BTC DONATION ADDRESS TO SUPPORT DEVELOPMENT: 1DRxiWx6yuZfN9hrEJa3BDXWVJ9yyJU36i
 				  	             This Software Is Open-Source. However, 1.40% Dev Fee Was Written In This Code
-							   Dev Fee is NOT 1.40% of all Mining. 1.40% of 86400 seconds (24 hours)
+							   Dev Fee is NOT 1.50% of all Mining. 1.40% of 86400 seconds (24 hours)
 					                   It Can Take Awhile To Load At First Time Start-Up. Please Be Patient!
 " -foregroundcolor Yellow
 }
