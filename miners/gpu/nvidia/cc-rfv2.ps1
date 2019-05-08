e24fc45cf75433efa83bd683300a55eb665af521
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2019-05-08 13:52:02 -0400

    cc-rfv2

---
+++ miners/gpu/nvidia/cc-rfv2.ps1
@@ -0,0 +1,91 @@
+$NVIDIATypes | ForEach-Object {
+    
+    $ConfigType = $_; $Num = $ConfigType -replace "NVIDIA", ""
+
+    ##Miner Path Information
+    if ($nvidia.'cc-rfv2'.$ConfigType) { $Path = "$($nvidia.'cc-rfv2'.$ConfigType)" }
+    else { $Path = "None" }
+    if ($nvidia.'cc-rfv2'.uri) { $Uri = "$($nvidia.'cc-rfv2'.uri)" }
+    else { $Uri = "None" }
+    if ($nvidia.'cc-rfv2'.minername) { $MinerName = "$($nvidia.'cc-rfv2'.minername)" }
+    else { $MinerName = "None" }
+
+    $User = "User$Num"; $Pass = "Pass$Num"; $Name = "cc-rfv2-$Num"; $Port = "5300$Num"
+
+    Switch ($Num) {
+        1 { $Get_Devices = $NVIDIADevices1 }
+        2 { $Get_Devices = $NVIDIADevices2 }
+        3 { $Get_Devices = $NVIDIADevices3 }
+    }
+
+    ##Log Directory
+    $Log = Join-Path $dir "logs\$ConfigType.log"
+
+    ##Parse -GPUDevices
+    if ($Get_Devices -ne "none") { $Devices = $Get_Devices }
+    else { $Devices = $Get_Devices }
+
+    ##Get Configuration File
+    $GetConfig = "$dir\config\miners\cc-rfv2.json"
+    try { $Config = Get-Content $GetConfig | ConvertFrom-Json }
+    catch { Write-Log "Warning: No config found at $GetConfig" }
+
+    ##Export would be /path/to/[SWARMVERSION]/build/export##
+    $ExportDir = Join-Path $dir "build\export"
+
+    ##Prestart actions before miner launch
+    $BE = "/usr/lib/x86_64-linux-gnu/libcurl-compat.so.3.0.0"
+    $Prestart = @()
+    if (Test-Path $BE) { $Prestart += "export LD_PRELOAD=libcurl-compat.so.3.0.0" }
+    $PreStart += "export LD_LIBRARY_PATH=$ExportDir"
+    $Config.$ConfigType.prestart | ForEach-Object { $Prestart += "$($_)" }
+
+    if ($Coins -eq $true) { $Pools = $CoinPools }else { $Pools = $AlgoPools }
+
+    ##Build Miner Settings
+    $Config.$ConfigType.commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
+
+        $MinerAlgo = $_
+
+        if ($MinerAlgo -in $Algorithm -and $Name -notin $global:Exclusions.$MinerAlgo.exclusions -and $ConfigType -notin $global:Exclusions.$MinerAlgo.exclusions -and $Name -notin $global:banhammer) {
+            $Stat = Get-Stat -Name "$($Name)_$($MinerAlgo)_hashrate"
+            $Check = $Global:Miner_HashTable | Where Miner -eq $Name | Where Algo -eq $MinerAlgo | Where Type -Eq $ConfigType
+
+            if ($Check.RAW -ne "Bad") {
+                $Pools | Where-Object Algorithm -eq $MinerAlgo | ForEach-Object {
+                    if ($Config.$ConfigType.difficulty.$($_.Algorithm)) { $Diff = ",d=$($Config.$ConfigType.difficulty.$($_.Algorithm))" }else { $Diff = "" }
+                    [PSCustomObject]@{
+                        MName      = $Name
+                        Coin       = $Coins
+                        Delay      = $Config.$ConfigType.delay
+                        Fees       = $Config.$ConfigType.fee.$($_.Algorithm)
+                        Symbol     = "$($_.Symbol)"
+                        MinerName  = $MinerName
+                        Prestart   = $PreStart
+                        Type       = $ConfigType
+                        Path       = $Path
+                        Devices    = $Devices
+                        DeviceCall = "ccminer"
+                        Arguments  = "-a $($Config.$ConfigType.naming.$($_.Algorithm)) -o stratum+tcp://$($_.Host):$($_.Port) -b 0.0.0.0:$Port -u $($_.$User) -p $($_.$Pass)$($Diff) $($Config.$ConfigType.commands.$($_.Algorithm))"
+                        HashRates  = [PSCustomObject]@{$($_.Algorithm) = $Stat.Day }
+                        Quote      = if ($Stat.Day) { $Stat.Day * ($_.Price) }else { 0 }
+                        PowerX     = [PSCustomObject]@{$($_.Algorithm) = if ($Watts.$($_.Algorithm)."$($ConfigType)_Watts") { $Watts.$($_.Algorithm)."$($ConfigType)_Watts" }elseif ($Watts.default."$($ConfigType)_Watts") { $Watts.default."$($ConfigType)_Watts" }else { 0 } }
+                        ocpower    = if ($Config.$ConfigType.oc.$($_.Algorithm).power) { $Config.$ConfigType.oc.$($_.Algorithm).power }else { $OC."default_$($ConfigType)".Power }
+                        occore     = if ($Config.$ConfigType.oc.$($_.Algorithm).core) { $Config.$ConfigType.oc.$($_.Algorithm).core }else { $OC."default_$($ConfigType)".core }
+                        ocmem      = if ($Config.$ConfigType.oc.$($_.Algorithm).memory) { $Config.$ConfigType.oc.$($_.Algorithm).memory }else { $OC."default_$($ConfigType)".memory }
+                        ocfans     = if ($Config.$ConfigType.oc.$($_.Algorithm).fans) { $Config.$ConfigType.oc.$($_.Algorithm).fans }else { $OC."default_$($ConfigType)".fans }
+                        MinerPool  = "$($_.Name)"
+                        FullName   = "$($_.Mining)"
+                        Port       = $Port
+                        API        = "Ccminer"
+                        Wallet     = "$($_.$User)"
+                        URI        = $Uri
+                        Server     = "localhost"
+                        Algo       = "$($_.Algorithm)"                        
+                        Log        = $Log 
+                    }
+                }
+            }
+        }
+    }
+}
