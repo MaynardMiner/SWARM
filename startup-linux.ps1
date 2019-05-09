b6a869430af102268e410989d702a97337eaa55c
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2019-05-08 21:48:14 -0400

    startup

--- startup-linux.ps1
+++ startup-linux.ps1
@@ -1,8 +1,7 @@
 Set-Location (Split-Path $script:MyInvocation.MyCommand.Path)
 $dir = Split-Path $script:MyInvocation.MyCommand.Path
 $json = $true
-try { $Parsed = $args | ConvertFrom-Json -ErrorAction Stop }catch { Write-Host "Could not convert from json, Trying different method"; $json = $false }
-if ($json -eq $false) {
+if ($args) {
     $global:parsed = @{ }
     $args | % {
         $Command = $false
@@ -19,9 +18,19 @@ if ($json -eq $false) {
         }
     }
 }
+elseif(Test-Path ".\config\parameters\arguments.json") {
+    $global:parsed = @{ }
+    $arguments = Get-Content ".\config\parameters\arguments.json" ConvertFrom-Json
+    $arguments.PSObject.Properties.Name | % { $Parsed.Add("$($_)", $arguments.$_) }
+}
+else{
+    Write-Host "No Arguments or arguments.json file found. Exiting."
+    Start-Sleep -S 3
+    exit
+}
 
 $Defaults = Get-Content ".\config\parameters\default.json" | ConvertFrom-Json
-$Defaults.PSObject.Properties.Name | % { if (-not $Parsed.$_) { $Parsed.Add("$($_)",$Defaults.$_) } }
+$Defaults.PSObject.Properties.Name | % { if (-not $Parsed.$_) { $Parsed.Add("$($_)", $Defaults.$_) } }
 
 $Parsed | convertto-json | Out-File ".\config\parameters\arguments.json"
 
