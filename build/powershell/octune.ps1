ab003ca6dc4409c7861fe428e1cb22133eb6fc9e
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2019-05-09 22:35:04 -0400

    export display

--- build/powershell/octune.ps1
+++ build/powershell/octune.ps1
@@ -231,7 +231,10 @@ function Start-OC {
     #OC For Devices
     $NVIDIAOCArgs = @(); $NVIDIAPowerArgs = @(); $NScript = @(); $AScript = @()
     $NScript += "`#`!/usr/bin/env bash"
-    if ($Global:Config.params.Platform -eq "linux") { $AScript += "`#`!/usr/bin/env bash" }
+    $NScript += "export DISPLAY=`":0`""
+    if ($Global:Config.params.Platform -eq "linux") {
+        $AScript += "`#`!/usr/bin/env bash" 
+    }
     if ($OC_Algo.Memory -or $OC_Algo.Core -or $OC_Algo.Fans) { $SettingsArgs = $true }
     elseif ($Default.Memory -or $Default.Core -or $Default.Fans) { $SettingsArgs = $true }
     if ($SettingsArgs -eq $true) { $NScript += "nvidia-settings " }
@@ -598,10 +601,10 @@ function Start-OC {
     }
     
     if ($DoNVIDIAOC -eq $true -and $Global:Config.params.Platform -eq "linux") {
-        if ($OCPOWERM) { $NScript[1] = "$($NScript[1])$OCPOWERM" }
-        if ($Core) { $NScript[1] = "$($NScript[1])$NVIDIACORE" }
-        if ($Mem) { $NScript[1] = "$($NScript[1])$NVIDIAMEM" }
-        if ($Fan) { $NScript[1] = "$($NScript[1])$NVIDIAFAN" }
+        if ($OCPOWERM) { $NScript[1] = "$($NScript[2])$OCPOWERM" }
+        if ($Core) { $NScript[1] = "$($NScript[2])$NVIDIACORE" }
+        if ($Mem) { $NScript[1] = "$($NScript[2])$NVIDIAMEM" }
+        if ($Fan) { $NScript[1] = "$($NScript[2])$NVIDIAFAN" }
         Start-Process "./build/bash/killall.sh" -ArgumentList "OC_NVIDIA" -Wait
         Start-Process "screen" -ArgumentList "-S OC_NVIDIA -d -m"
         Start-Sleep -S 1
