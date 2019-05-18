b572570fb15ca671b54bec9a5a254f39d79e4199
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2019-05-18 19:19:56 -0400

    Update cgminer-bfgminer.ps1

--- miners/asic/cgminer-bfgminer.ps1
+++ miners/asic/cgminer-bfgminer.ps1
@@ -39,7 +39,7 @@ $ASICTypes | ForEach-Object {
                     Wallet     = "$($_.$User)"
                     Arguments  = "stratum+tcp://$($_.Host):$($_.Port),$($_.$User),$Pass"
                     HashRates  = [PSCustomObject]@{$($_.Algorithm) = $Stat.Hour}
-                    Quote      = if ($Stat.Day) { $Stat.Day * ($_.Price) }else { 0 }
+                    Quote      = if ($Stat.Hour) { $Stat.Hour * ($_.Price) }else { 0 }
                     PowerX     = [PSCustomObject]@{$($_.Algorithm) = if ($Watts.$($_.Algorithm)."$($ConfigType)_Watts") { $Watts.$($_.Algorithm)."$($ConfigType)_Watts" }elseif ($Watts.default."$($ConfigType)_Watts") { $Watts.default."$($ConfigType)_Watts" }else { 0 } }
                     MinerPool  = "$($_.Name)"
                     FullName   = "$($_.Mining)"
