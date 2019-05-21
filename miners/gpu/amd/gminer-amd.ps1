99bc2e25503a7bb3100a3177b27cbdb2eb320070
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2019-05-20 21:39:36 -0400

    bad push

--- miners/gpu/amd/gminer-amd.ps1
+++ miners/gpu/amd/gminer-amd.ps1
@@ -70,8 +70,10 @@ $AMDTypes | ForEach-Object {
                     $SelAlgo = $_.Algorithm
                     switch($SelAlgo) {
                         "equihash_150/5" {$AddArgs = "--algo 150_5 --pers auto "}
+                        "cuckoo_cycle" {$AddArgs = "--algo aeternity "}
                         "cuckaroo29" {$AddArgs = "--algo grin29 "}
                         "cuckatoo31" {$AddArgs = "--algo grin31 "}
+                        "equihash_96/5" {$AddArgs = "--algo 96_5 --pers auto "}
                         "equihash_192/7" {$AddArgs = "--algo 192_7 --pers auto "}
                         "equihash_144/5" {$AddArgs = "--algo 144_5 --pers auto "}
                         "equihash_210/9" {$AddArgs = "--algo 210_9 --pers auto "}
