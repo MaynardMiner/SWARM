4d01bcc5070bbdc080b736bb571237eb6f6f993b
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2019-06-02 12:03:47 -0400

    -No_GLT argument

--- coinpools/zergpool.ps1
+++ coinpools/zergpool.ps1
@@ -6,6 +6,7 @@ $Zergpool_UnSorted = [PSCustomObject]@{ }
 $DoAutoCoin = $false
 if($global:Config.Params.Coin.Count -eq 0){$DoAutoCoin = $true}
 $global:Config.Params.Coin | %{ if($_ -eq ""){$DoAutoCoin = $true}}
+if($Global:Config.Params.NO_GLT -eq "Yes"){$NoGLT = "GLT"}
 
 if ($global:Config.Params.xnsub -eq "Yes") { $X = "#xnsub" } 
 
@@ -55,6 +56,7 @@ if ($Name -in $global:Config.Params.PoolName) {
             Where-Object { $global:Config.Pool_Algos.$($_.Algo) } |
             Where-Object { $Name -notin $global:Config.Pool_Algos.$($_.Algo).exclusions }  |
             Where-Object Sym -notin $global:BanHammer |
+            Where-Object Sym -notlike "*$NoGLT*" |
             Where-Object noautotrade -eq "0" | 
             Where-Object estimate -gt 0 | 
             Where-Object hashrate -ne 0 | 
@@ -77,6 +79,7 @@ if ($Name -in $global:Config.Params.PoolName) {
             Where-Object { $global:Config.Pool_Algos.$($_.Algo) } |
             Where-Object { $Name -notin $global:Config.Pool_Algos.$($_.sym).exclusions }  |
             Where-Object Sym -notin $global:BanHammer |
+            Where-Object Sym -notlike "*$NoGLT*" |
             Where-Object noautotrade -eq "0" |
             Where-Object estimate -gt 0 |
             Where-Object hashrate -ne 0 |
