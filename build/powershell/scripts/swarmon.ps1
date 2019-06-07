d7b8096262d353548f44676c0632abaff9c52aeb
maynardminer <maynardviii@gmail.com>
2019-06-07 17:42:52 -0400

    small fix

--- build/powershell/scripts/swarmon.ps1
+++ build/powershell/scripts/swarmon.ps1
@@ -6,14 +6,15 @@ param(
 if($Action) {
     $SMOS_CONFIG = "/root/utils/update_register.sh"
     if(test-path ($SMOS_CONFIG)){ $Changed = Get-Content $SMOS_CONFIG }    
-
+    $Save = $False
     switch($Action) {
         "on" {
-            $Changed = $Changed -replace "    /root/utils/update_configGet.sh","    /root/utils/update_configGet.sh`n    if [ -f /root/swarm_mode.txt ]; then`n    SWARM_MODE=``cat /miners/SWARM_MODE```n        if [ `"`$SWARM_MODE`" = `"Yes`"]; then`n            pwsh -command `"./root/SWARM/build/powershell/scripts/smos_config.ps1`"`n        fi`n    fi"
+            if(-not ($Changed | Select-String "SWARM_MODE")){ $Changed = $Changed -replace "    /root/utils/update_configGet.sh","    /root/utils/update_configGet.sh`n    if [ -f /root/swarm_mode.txt ]; then`n    SWARM_MODE=``cat /miners/SWARM_MODE```n        if [ `"`$SWARM_MODE`" = `"Yes`"]; then`n            pwsh -command `"./root/SWARM/build/powershell/scripts/smos_config.ps1`"`n        fi`n    fi"; $Save = $True }
+            "Yes" | Set-Content "/root/swarm_mode.txt"
         }
         "off" {
-            $Changed = $Changed.Replace("    if [ -f /root/swarm_mode.txt ]; then`n    SWARM_MODE=``cat /miners/SWARM_MODE```n        if [ `"`$SWARM_MODE`" = `"Yes`"]; then`n            pwsh -command `"./root/SWARM/build/powershell/scripts/smos_config.ps1`"`n        fi`n    fi","")
+            "No" | Set-Content "/root/swarm_mode.txt"
         }
     }
-    $Changed | Set-Content "$SMOS_CONFIG"
+    if($Save -eq $True){ $Changed | Set-Content "$SMOS_CONFIG" }
 }
