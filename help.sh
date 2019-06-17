f1956bb77af58e1b6f5b1d48283cb576332b89a8
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2019-06-16 23:32:12 -0400

    bash help

---
+++ help.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if ! [ -x "$(command -v pwsh)" ]; then
+wget https://github.com/PowerShell/PowerShell/releases/download/v6.1.0/powershell-6.1.0-linux-x64.tar.gz -O /tmp/powershell.tar.gz
+mkdir -p /opt/microsoft/powershell/6.1.0
+tar zxf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/6.1.0
+chmod +x /opt/microsoft/powershell/6.1.0/pwsh
+ln -s /opt/microsoft/powershell/6.1.0/pwsh /usr/bin/pwsh
+rm -rf /tmp/powershell.tar.gz
+fi
+pwsh -command "./install.ps1"
+chmod 777 -R $HOME/.local/share/powershell
+pwsh -command "./help.ps1"
