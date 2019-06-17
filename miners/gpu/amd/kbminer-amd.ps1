ab10b0c249c91f49a61913a0a263896af26734ba
maynardminer <maynardviii@gmail.com>
2019-06-17 19:07:22 -0400

    kbminer (no api yet)

---
+++ miners/gpu/amd/kbminer-amd.ps1
@@ -0,0 +1,88 @@
+$(vars).AMDTypes | ForEach-Object {
+    
+    $ConfigType = $_; $Num = $ConfigType -replace "AMD", ""
+    
+    $CName = "kbminer-amd"
+
+    ##Miner Path Information
+    if ($Global:amd.$CName.$ConfigType) { $Path = "$($Global:amd.$CName.$ConfigType)" }
+    else { $Path = "None" }
+    if ($Global:amd.$CName.uri) { $Uri = "$($Global:amd.$CName.uri)" }
+    else { $Uri = "None" }
+    if ($Global:amd.$CName.minername) { $MinerName = "$($Global:amd.$CName.minername)" }
+    else { $MinerName = "None" }
+
+    $User = "User$Num"; $Pass = "Pass$Num"; $Name = "$CName-$Num"; $Port = "3600$Num"
+
+    Switch ($Num) {
+        1 { $Get_Devices = $(vars).AMDDevices1; $Rig = $(arg).Rigname1 }
+    }
+
+    ##Log Directory
+    $Log = Join-Path $($(vars).dir) "logs\$ConfigType.log"
+
+    ##Parse -GPUDevices
+    if ($Get_Devices -ne "none") { $Devices = $Get_Devices }
+    else { $Devices = $Get_Devices }
+
+    ##Get Configuration File
+    $MinerConfig = $Global:config.miners.$CName
+
+    ##Export would be /path/to/[SWARMVERSION]/build/export##
+    $ExportDir = Join-Path $($(vars).dir) "build\export"
+
+    ##Prestart actions before miner launch
+    $BE = "/usr/lib/x86_64-linux-gnu/libcurl-compat.so.3.0.0"
+    $Prestart = @()
+    if (Test-Path $BE) { $Prestart += "export LD_PRELOAD=libcurl-compat.so.3.0.0" }
+    $PreStart += "export LD_LIBRARY_PATH=./:$ExportDir"
+    $MinerConfig.$ConfigType.prestart | ForEach-Object { $Prestart += "$($_)" }
+
+    if ($Global:Coins -eq $true) { $Pools = $global:CoinPools }else { $Pools = $global:AlgoPools }
+
+    ##Build Miner Settings
+    $MinerConfig.$ConfigType.commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
+
+        $MinerAlgo = $_
+
+        if ($MinerAlgo -in $global:Algorithm -and $Name -notin $global:Config.Pool_Algos.$MinerAlgo.exclusions -and $ConfigType -notin $global:Config.Pool_Algos.$MinerAlgo.exclusions -and $Name -notin $global:banhammer) {
+            $StatAlgo = $MinerAlgo -replace "`_", "`-"
+            $Stat = Global:Get-Stat -Name "$($Name)_$($StatAlgo)_hashrate" 
+            $Check = $Global:Miner_HashTable | Where Miner -eq $Name | Where Algo -eq $MinerAlgo | Where Type -Eq $ConfigType
+        
+            if ($Check.RAW -ne "Bad") {
+                $Pools | Where-Object Algorithm -eq $MinerAlgo | ForEach-Object {
+                    if ($MinerConfig.$ConfigType.difficulty.$($_.Algorithm)) { $Diff = ",d=$($MinerConfig.$ConfigType.difficulty.$($_.Algorithm))" }else { $Diff = "" }
+                    [PSCustomObject]@{
+                        MName      = $Name
+                        Coin       = $Global:Coins
+                        Delay      = $MinerConfig.$ConfigType.delay
+                        Fees       = $MinerConfig.$ConfigType.fee.$($_.Algorithm)
+                        Symbol     = "$($_.Symbol)"                    
+                        MinerName  = $MinerName                    
+                        Prestart   = $PreStart
+                        Type       = $ConfigType
+                        Path       = $Path
+                        Devices    = $Devices
+                        Stratum    = "$($_.Protocol)://$($_.Host):$($_.Port)" 
+                        Version    = "$($Global:amd.$CName.version)"
+                        DeviceCall = "kbminer"
+                        Arguments  = "--algorithm $($MinerConfig.$ConfigType.naming.$($_.Algorithm)) --enable-api --apiaddr $Port --pool stratum+tcp://$($_.Host):$($_.Port) --user $($_.$User) --pass $($_.$Pass)$($Diff) $($MinerConfig.$ConfigType.commands.$($_.Algorithm))"
+                        HashRates  = $Stat.Hour
+                        Quote      = if ($Stat.Hour) { $Stat.Hour * ($_.Price) }else { 0 }
+                        Power      = if ($(vars).Watts.$($_.Algorithm)."$($ConfigType)_Watts") { $(vars).Watts.$($_.Algorithm)."$($ConfigType)_Watts" }elseif ($(vars).Watts.default."$($ConfigType)_Watts") { $(vars).Watts.default."$($ConfigType)_Watts" }else { 0 } 
+                        MinerPool  = "$($_.Name)"
+                        Port       = $Port
+                        Worker     = $Rig
+                        API        = "kbminer"
+                        Wallet     = "$($_.$User)"
+                        URI        = $Uri
+                        Server     = "localhost"
+                        Algo       = "$($_.Algorithm)"                         
+                        Log        = $Log 
+                    }            
+                }
+            }
+        }
+    }
+}
