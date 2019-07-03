318fdbc886e909773f3f74e2f35a9fe4e1148ffe
maynardminer <maynardviii@gmail.com>
2019-07-02 20:59:50 -0400

    extra in klauT

--- miners/optional_and_old/klaust.ps1
+++ miners/optional_and_old/klaust.ps1
@@ -54,36 +54,34 @@ $(vars).NVIDIATypes | ForEach-Object {
         
             if ($Check.RAW -ne "Bad") {
                 $Pools | Where-Object Algorithm -eq $MinerAlgo | ForEach-Object {
-                    if ($(vars).Algorithm -eq "$($_.Algorithm)") {
-                        if ($MinerConfig.$ConfigType.difficulty.$($_.Algorithm)) { $Diff = ",d=$($MinerConfig.$ConfigType.difficulty.$($_.Algorithm))" }else { $Diff = "" }
-                        [PSCustomObject]@{
-                            MName      = $Name
-                            Coin       = $(vars).Coins
-                            Delay      = $MinerConfig.$ConfigType.delay
-                            Fees       = $MinerConfig.$ConfigType.fee.$($_.Algorithm)
-                            Symbol     = "$($_.Symbol)"
-                            MinerName  = $MinerName
-                            Prestart   = $PreStart
-                            Type       = $ConfigType
-                            Path       = $Path
-                            Devices    = $Devices
-                            Stratum    = "$($_.Protocol)://$($_.Host):$($_.Port)" 
-                            Version    = "$($(vars).nvidia.klaust.version)"
-                            DeviceCall = "ccminer"
-                            Arguments  = "-a $($MinerConfig.$ConfigType.naming.$($_.Algorithm)) -o stratum+tcp://$($_.Host):$($_.Port) -b 0.0.0.0:$Port -u $($_.$User) -p $($_.$Pass)$($Diff) $($MinerConfig.$ConfigType.commands.$($_.Algorithm))"
-                            HashRates  = $Stat.Hour
-                            Quote      = if ($Stat.Hour) { $Stat.Hour * ($_.Price) }else { 0 }
-                            Power      = if ($(vars).Watts.$($_.Algorithm)."$($ConfigType)_Watts") { $(vars).Watts.$($_.Algorithm)."$($ConfigType)_Watts" }elseif ($(vars).Watts.default."$($ConfigType)_Watts") { $(vars).Watts.default."$($ConfigType)_Watts" }else { 0 } 
-                            MinerPool  = "$($_.Name)"
-                            Port       = $Port
-                            Worker     = $Rig
-                            API        = "Ccminer"
-                            Wallet     = "$($_.$User)"
-                            URI        = $Uri
-                            Server     = "localhost"
-                            Algo       = "$($_.Algorithm)"
-                            Log        = $Log 
-                        }
+                    if ($MinerConfig.$ConfigType.difficulty.$($_.Algorithm)) { $Diff = ",d=$($MinerConfig.$ConfigType.difficulty.$($_.Algorithm))" }else { $Diff = "" }
+                    [PSCustomObject]@{
+                        MName      = $Name
+                        Coin       = $(vars).Coins
+                        Delay      = $MinerConfig.$ConfigType.delay
+                        Fees       = $MinerConfig.$ConfigType.fee.$($_.Algorithm)
+                        Symbol     = "$($_.Symbol)"
+                        MinerName  = $MinerName
+                        Prestart   = $PreStart
+                        Type       = $ConfigType
+                        Path       = $Path
+                        Devices    = $Devices
+                        Stratum    = "$($_.Protocol)://$($_.Host):$($_.Port)" 
+                        Version    = "$($(vars).nvidia.klaust.version)"
+                        DeviceCall = "ccminer"
+                        Arguments  = "-a $($MinerConfig.$ConfigType.naming.$($_.Algorithm)) -o stratum+tcp://$($_.Host):$($_.Port) -b 0.0.0.0:$Port -u $($_.$User) -p $($_.$Pass)$($Diff) $($MinerConfig.$ConfigType.commands.$($_.Algorithm))"
+                        HashRates  = $Stat.Hour
+                        Quote      = if ($Stat.Hour) { $Stat.Hour * ($_.Price) }else { 0 }
+                        Power      = if ($(vars).Watts.$($_.Algorithm)."$($ConfigType)_Watts") { $(vars).Watts.$($_.Algorithm)."$($ConfigType)_Watts" }elseif ($(vars).Watts.default."$($ConfigType)_Watts") { $(vars).Watts.default."$($ConfigType)_Watts" }else { 0 } 
+                        MinerPool  = "$($_.Name)"
+                        Port       = $Port
+                        Worker     = $Rig
+                        API        = "Ccminer"
+                        Wallet     = "$($_.$User)"
+                        URI        = $Uri
+                        Server     = "localhost"
+                        Algo       = "$($_.Algorithm)"
+                        Log        = $Log 
                     }
                 }
             }
