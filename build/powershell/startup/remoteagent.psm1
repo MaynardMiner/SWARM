dfcb6f4c4fd4932618722e9cb5dda2d312cfbef0
maynardminer <maynardviii@gmail.com>
2019-08-02 03:01:37 -0400

    bump version

--- build/powershell/startup/remoteagent.psm1
+++ build/powershell/startup/remoteagent.psm1
@@ -45,6 +45,7 @@ function Global:start-update {
         $PreviousVersions += "SWARM.2.5.1"
         $PreviousVersions += "SWARM.2.5.2"
         $PreviousVersions += "SWARM.2.5.3"
+        $PreviousVersions += "SWARM.2.5.4"
 
         $StatsOnly = $null
 
