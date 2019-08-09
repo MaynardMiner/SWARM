05e840f317fda86e6c134e3aa0ddcca46c238cb4
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2019-08-08 22:22:26 -0400

    force double

--- build/powershell/global/stats.psm1
+++ build/powershell/global/stats.psm1
@@ -20,7 +20,7 @@ function Global:Start-Shuffle($X, $Y) {
     $historical_P = ($(arg).historical_bias / 100)
     $historical_N = ($(arg).historical_bias / 100) * -1
     if( $X -gt $historical_P){ $X = $historical_P }
-    if($X -lt $historical_N -and $(arg).historical_bias -ne 0){
+    if($X -lt $historical_N ){
         if($X -le -1){ $X = -1 }
         else{ $X = $historical_N }
     }
