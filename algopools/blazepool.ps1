a6e34935418480a089378a174061740ce42344f4
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2019-08-08 22:32:34 -0400

    force double

--- algopools/blazepool.ps1
+++ algopools/blazepool.ps1
@@ -29,7 +29,7 @@ if ($Name -in $(arg).PoolName) {
     ## Add 24 hour deviation.
     $Pool_Sorted | ForEach-Object {
         $Raw = shuffle $_.estimate_last24h $_.actual_last24h
-        $_ | Add-Member "deviation" $Raw
+        $_ | Add-Member "deviation" [Double]$Raw
     }
 
     $Pool_Sorted | ForEach-Object {
