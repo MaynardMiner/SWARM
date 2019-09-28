c1985b95de4791fb3a6d4bbdbfb7cb2a9e022802
maynardminer <maynardviii@gmail.com>
2019-09-27 20:42:20 -0400

    benchmark has changed to bench

--- build/powershell/benchmark/attempt.psm1
+++ build/powershell/benchmark/attempt.psm1
@@ -233,12 +233,12 @@ function Global:Start-Benchmark {
                                     if (-not (Test-Path $NewHashrateFilePath)) {
                                         Copy-Item $HashrateFilePath -Destination $NewHashrateFilePath -force
                                         log "$($_.Name) $($_.Symbol) Was Benchmarked And Backed Up" -foregroundcolor yellow
-                                        log "Was this stat not correct? You can run command 'bench miner $($_.Name)' or 'benchmark algorithm $($_.algo)' to reset benchmark`n" -foregroundcolor cyan
+                                        log "Was this stat not correct? You can run command 'bench miner $($_.Name)' or 'bench algorithm $($_.algo)' to reset benchmark`n" -foregroundcolor cyan
                                     }
                                     $global:WasBenchmarked = $True
                                     Global:Get-Intensity $_.Type $_.Symbol $_.Path
                                     log "Stat Written" -foregroundcolor green
-                                    log "Was this stat not correct? You can run command 'bench miner $($_.Name)' or 'benchmark algorithm $($_.algo)' to reset benchmark`n" -foregroundcolor cyan
+                                    log "Was this stat not correct? You can run command 'bench miner $($_.Name)' or 'bench algorithm $($_.algo)' to reset benchmark`n" -foregroundcolor cyan
                                     $Global:Strike = $false
                                 } 
                             }
