545af60ade11d2527e4f7138ef783565e382345b
maynardminer <maynardviii@gmail.com>
2019-11-20 23:06:19 -0500

    bash dir change   change dir for build/txt to debug

--- build/bash/rocm.sh
+++ build/bash/rocm.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 mydir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-cd $(< $mydir/dir.sh)/build/txt
+cd $(< $mydir/dir.sh)/debug
 timeout -s9 30 rocm-smi -P > amdpower.txt
