92619aa358ba12f53253e444a67ca3297566e00b
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2019-11-24 14:46:55 -0500

    #226 Pause Thread

--- swarm.ps1
+++ swarm.ps1
@@ -30,6 +30,9 @@ if($PSVersionTable.PSVersion -ne "6.2.3") {
     Write-Host "WARNING: Powershell Core Version is $($PSVersionTable.PSVersion)" -ForegroundColor Red
     Write-Host "Currently supported version for SWARM is 6.2.3" -ForegroundColor Red
     Write-Host "SWARM will continue anyways- It may cause issues." -ForegroundColor Red
+    ## Create a pause in case window is scrolling too fast.
+    Start-Sleep -S 10
+
 }
 
 ##filepath dir
