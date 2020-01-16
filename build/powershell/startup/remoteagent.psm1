fcc4de1540f480449a3a95f59a5b119a04f4a82a
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2020-01-16 18:40:27 -0500

    cpu only

--- build/powershell/startup/remoteagent.psm1
+++ build/powershell/startup/remoteagent.psm1
@@ -196,11 +196,6 @@ function Global:start-update {
                                             $Data.$_.naming | Add-Member "random-sfx" "rx/sfx" -ErrorAction SilentlyContinue
                                             $Data.$_.fee | Add-Member "random-sfx" 1 -ErrorAction SilentlyContinue
 
-                                            $Data.$_.commands | Add-Member "chukwa" "" -ErrorAction SilentlyContinue
-                                            $Data.$_.difficulty | Add-Member "chukwa" "" -ErrorAction SilentlyContinue 
-                                            $Data.$_.naming | Add-Member "chukwa" "chukwa" -ErrorAction SilentlyContinue
-                                            $Data.$_.fee | Add-Member "chukwa" 1 -ErrorAction SilentlyContinue
-
                                             $Data.$_.commands = $Data.$_.commands | Select-Object -ExcludeProperty "randomv","randomsfx","cryptonight_gpu","cryptonight_xeq"
                                             $Data.$_.difficulty = $Data.$_.difficulty | Select-Object -ExcludeProperty "randomv","randomsfx","cryptonight_gpu","cryptonight_xeq"
                                             $Data.$_.naming = $Data.$_.naming | Select-Object -ExcludeProperty "randomv","randomsfx","cryptonight_gpu","cryptonight_xeq"
@@ -311,11 +306,6 @@ function Global:start-update {
                                             $Data.$_.difficulty | Add-Member "random-sfx" "" -ErrorAction SilentlyContinue 
                                             $Data.$_.naming | Add-Member "random-sfx" "rx/sfx" -ErrorAction SilentlyContinue
                                             $Data.$_.fee | Add-Member "random-sfx" 1 -ErrorAction SilentlyContinue
-
-                                            $Data.$_.commands | Add-Member "chukwa" "" -ErrorAction SilentlyContinue
-                                            $Data.$_.difficulty | Add-Member "chukwa" "" -ErrorAction SilentlyContinue 
-                                            $Data.$_.naming | Add-Member "chukwa" "chukwa" -ErrorAction SilentlyContinue
-                                            $Data.$_.fee | Add-Member "chukwa" 1 -ErrorAction SilentlyContinue
 
                                             $Data.$_.commands = $Data.$_.commands | Select-Object -ExcludeProperty "randomv","randomsfx","cryptonight_gpu","cryptonight_xeq"
                                             $Data.$_.difficulty = $Data.$_.difficulty | Select-Object -ExcludeProperty "randomv","randomsfx","cryptonight_gpu","cryptonight_xeq"
