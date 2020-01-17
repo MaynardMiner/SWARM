48965b34ece310effae72aeadaefa0fb9d5e54e9
maynardminer <maynardviii@gmail.com>
2020-01-17 17:13:03 -0500

    git update index for .sh files in main directory.   -chmod +x files.

--- install_linux.sh
+++ install_linux.sh
@@ -14,6 +14,7 @@
 # in case they need to be done manually rather than using powershell code.
 
 if ! [ -x "$(command -v pwsh)" ]; then
+echo 'pwsh not found- installing 6.2.3'
 wget https://github.com/PowerShell/PowerShell/releases/download/v6.2.3/powershell-6.2.3-linux-x64.tar.gz -O /tmp/powershell.tar.gz --no-check-certificate
 mkdir -p /opt/microsoft/powershell/6.2.3
 tar zxf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/6.2.3
@@ -22,4 +23,5 @@ ln -s /opt/microsoft/powershell/6.2.3/pwsh /usr/bin/pwsh
 rm -rf /tmp/powershell.tar.gz
 fi
 chmod 777 -R $HOME/.local/share/powershell
+echo 'starting install script'
 pwsh -command "./build/powershell/scripts/install.ps1"
