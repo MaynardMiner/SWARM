6ba8d49171e2b764822ad0093118924bdc2c9916
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2020-02-09 21:08:43 -0500

    Add quotes

--- SWARM Terminal.bat
+++ SWARM Terminal.bat
@@ -4,15 +4,15 @@ ECHO You can run terminal commands here.
 ECHO Commands such as:
 echo.       
 echo.       
-ECHO       get stats (gets miner stats)
-ECHO       get active (gets mining history)
-ECHO       get help (shows most commands and usage)
-ECHO       bench bans (resets all AI bans)
-ECHO       version query (lists all miners and their versions)
-ECHO       version update (updates a current miner)
-ECHO       swarm_help (starts configuration help)
-ECHO       clear_profits (deletes all profit data from pools)
-ECHO       clear_watts (removes all saved wattometer readings)
+ECHO       'get stats' (gets miner stats)
+ECHO       'get active' (gets mining history)
+ECHO       'get help' (shows most commands and usage)
+ECHO       'bench bans' (resets all AI bans)
+ECHO       'version query' (lists all miners and their versions)
+ECHO       'version update' (updates a current miner)
+ECHO       'swarm_help' (starts configuration help)
+ECHO       'clear_profits' (deletes all profit data from pools)
+ECHO       'clear_watts' (removes all saved wattometer readings)
 ECHO
 ECHO       Note- all these commands can be ran and auto refreshed
 ECHO       Using 'nview' command
@@ -22,8 +22,8 @@ ECHO       nview -n 30 -OnChange get stats 5
 ECHO
 ECHO       This command will run get stats 5 every 30 seconds
 ECHO       and refresh screen if data has changed.
-ECHO       get stats 5 will get top 5 stats.
-ECHO       get stats lite will get stats in Cell-Phone
+ECHO       'get stats 5' will get top 5 stats.
+ECHO       'get stats lite' will get stats in Cell-Phone
 ECHO       friendly format (List format)
 echo.       
 echo.       
