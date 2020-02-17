1e10435dbadad5edc91242e045834acc35a1e748
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2020-02-17 17:21:12 -0500

    update miner.ps1

--- build/powershell/scripts/miner.ps1
+++ build/powershell/scripts/miner.ps1
@@ -15,33 +15,42 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 $AllProtocols = [System.Net.SecurityProtocolType]'Tls,Tls11,Tls12' 
 [System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
 $dir = (Split-Path (Split-Path (Split-Path (Split-Path $script:MyInvocation.MyCommand.Path))))
-$dir = $dir -replace "/var/tmp","/root"
+$dir = $dir -replace "/var/tmp", "/root"
 Set-Location $dir
 
 $Trigger = $args
-$Id = if(Test-Path ".\build\pid\miner_pid.txt"){cat ".\build\pid\miner_pid.txt"}
+$Id = if (Test-Path ".\build\pid\miner_pid.txt") { cat ".\build\pid\miner_pid.txt" }
 
-Switch($Args) {
+Switch ($Args) {
     "restart" {
-        if($Id) {
+        if ($Id) {
             Write-Host "Searching for SWARM Process ID $ID"
             $Found = Get-Process | Where Id -eq $Id
-            if($found) {
+            if ($found) {
                 Write-Host "Found running SWARM process- Stopping"
-                Stop-Process -Id $Found.Id -Force -ErrorAction SilentlyContinue
-            } else { Write-Host "Could not find running SWARM Process"}      
+                if ($ISWindows) { $Found.CloseMainWindow() }
+                else {
+                    Stop-Process -Id $Found.Id -Force -ErrorAction SilentlyContinue
+                }
+            }
+            else { Write-Host "Could not find running SWARM Process" }      
             Write-Host "Starting SWARM"      
             Start-Process ".\SWARM.bat"
-        } else {Write-Host "No SWARM process found"}
+        }
+        else { Write-Host "No SWARM process found" }
     }
     "stop" {
-        if($Id) {
+        if ($Id) {
             Write-Host "Searching for SWARM Process ID $ID"
             $Found = Get-Process | Where Id -eq $Id
-            if($found) {
+            if ($found) {
                 Write-Host "Found running SWARM process- Stopping"
-                Stop-Process -Id $Found.Id -Force -ErrorAction SilentlyContinue
-            } else { Write-Host "Could not find running SWARM Process"}      
+                if ($ISWindows) { $Found.CloseMainWindow() }
+                else {
+                    Stop-Process -Id $Found.Id -Force -ErrorAction SilentlyContinue
+                }
+            }
+            else { Write-Host "Could not find running SWARM Process" }      
         }
     }
     "start" {
@@ -50,10 +59,11 @@ Switch($Args) {
     }
     "log" {
         $Active = Get-ChildItem ".\logs" | Where BaseName -like "*Active*" | Select -First 1
-        if($Active) {
+        if ($Active) {
             $Log = Get-Content $Active
             $Log | Out-Host
-        } else {Write-Host "No Active Miner Log Found"}
+        }
+        else { Write-Host "No Active Miner Log Found" }
     }
     default {
         Write-Host "Usage stop|start|restart|log"
