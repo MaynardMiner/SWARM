c5d57246e051c826850e2a1497f08da50a3ca82d
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2020-04-10 15:58:14 -0400

    initial test average hashrates/watts

--- build/powershell/benchmark/attempt.psm1
+++ build/powershell/benchmark/attempt.psm1
@@ -172,7 +172,7 @@ function Global:Start-Benchmark {
                     $Global:Strike = $True
                 }
                 for ($i = 0; $i -lt 4; $i++) {
-                    $Miner_HashRates = Global:Get-HashRate -Type $_.Type
+                    $Miner_HashRates = $(Vars).Hashtable.$($_.Type).Hashrate;
                     $_.HashRate = $Miner_HashRates
                     if ($global:WasBenchmarked -eq $False) {
                         $HashRateFilePath = Join-Path ".\stats" "$($_.Name)_$($NewName)_hashrate.txt"
@@ -201,7 +201,7 @@ function Global:Start-Benchmark {
                         }
                         else {
                             $No_Watts = @("CPU","ASIC")
-                            if ($(arg).WattOMeter -eq "Yes" -and $_.Type -notin $No_Watts) { $GPUPower = Global:Get-Power $($_.Type) }
+                            if ($(arg).WattOMeter -eq "Yes" -and $_.Type -notin $No_Watts) { $GPUPower = $(vars).$($_.Type).watts; }
                             else { 
                                 $GPUPower = 1 
                             }
