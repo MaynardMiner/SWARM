6f334807d9ea7090f8a68ac58eace0e99b5ad5eb
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2020-04-16 20:26:57 -0400

    srbmulti-   just do k12. Rest of algorithms and api is being difficult.   no better than miners that are currently in   SWARM

--- build/api/miners/srbmulti.psm1
+++ build/api/miners/srbmulti.psm1
@@ -4,7 +4,7 @@ function Global:Get-StatsSrbmulti {
         try { $Data = $Request.Content | ConvertFrom-Json -ErrorAction Stop }
         Catch { Write-Host "Failed To parse API" -ForegroundColor Red; Break }
         if ($Data) {
-            $global:RAW += $Data.hashrate_total_1min;
+            $global:RAW += $Data.hashrate_total_now / 1000;
             $Hash = @()
             Global:Write-MinerData2;
             $Data.gpu_hashrate | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
@@ -15,13 +15,13 @@ function Global:Get-StatsSrbmulti {
             
             try {
                 for ($global:i = 0; $global:i -lt $Devices.Count; $global:i++) { 
-                    $global:GPUHashrates.$(Global:Get-GPUs) = (Global:Set-Array $Hash $global:i) / 1000 
+                    $global:GPUHashrates.$(Global:Get-GPUs) = (Global:Set-Array $Hash $global:i) / 1000000 
                 }
             }
             catch { Write-Host "Failed To parse Threads" -ForegroundColor Red };
             $global:MinerACC = $Data.shares.accepted; $global:ALLACC += $Data.shares.accepted 
             $global:MinerREJ = $Data.shares.rejected; $global:ALLREG += $Data.shares.accepted
-            $Hash | ForEach-Object { $global:GPUKHS += [Double]$_ / 1000 }
+            $Hash | ForEach-Object { $global:GPUKHS += [Double]$_ / 1000000 }
         }
     }
     else { Global:Set-APIFailure }
