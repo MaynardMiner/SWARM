74d2c09a9f6a7c9333bac74734a2f8e9403c6640
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2020-05-20 20:34:20 -0400

    Powershell update   -auto-update to 7.0.1 (linux only)

--- h-run.sh
+++ h-run.sh
@@ -33,27 +33,27 @@ disk-expand
 rm -rf /opt/microsoft/powershell/
 rm -rf /usr/bin/pwsh
 rm -rf /usr/bin/pwsh-preview
-wget https://github.com/PowerShell/PowerShell/releases/download/v7.0.0/powershell-7.0.0-linux-x64.tar.gz -O /tmp/powershell.tar.gz --no-check-certificate
-mkdir -p /opt/microsoft/powershell/7.0.0
-tar zxf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/7.0.0
-chmod +x /opt/microsoft/powershell/7.0.0/pwsh
-ln -s /opt/microsoft/powershell/7.0.0/pwsh /usr/bin/pwsh
+wget https://github.com/PowerShell/PowerShell/releases/download/v7.0.1/powershell-7.0.1-linux-x64.tar.gz -O /tmp/powershell.tar.gz --no-check-certificate
+mkdir -p /opt/microsoft/powershell/7.0.1
+tar zxf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/7.0.1
+chmod +x /opt/microsoft/powershell/7.0.1/pwsh
+ln -s /opt/microsoft/powershell/7.0.1/pwsh /usr/bin/pwsh
 rm -rf /tmp/powershell.tar.gz
 fi
 
 PVERSION=`pwsh -version`
 
 ## If pwsh is wrong version, install it again.
-if [ "$PVERSION" != "PowerShell 7.0.0" ]; then
+if [ "$PVERSION" != "PowerShell 7.0.1" ]; then
 echo "updating powershell to latest version"
 rm -rf /opt/microsoft/powershell/
 rm -rf /usr/bin/pwsh
 rm -rf /usr/bin/pwsh-preview
-wget https://github.com/PowerShell/PowerShell/releases/download/v7.0.0/powershell-7.0.0-linux-x64.tar.gz -O /tmp/powershell.tar.gz --no-check-certificate
-mkdir -p /opt/microsoft/powershell/7.0.0
-tar zxf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/7.0.0
-chmod +x /opt/microsoft/powershell/7.0.0/pwsh
-ln -s /opt/microsoft/powershell/7.0.0/pwsh /usr/bin/pwsh
+wget https://github.com/PowerShell/PowerShell/releases/download/v7.0.1/powershell-7.0.1-linux-x64.tar.gz -O /tmp/powershell.tar.gz --no-check-certificate
+mkdir -p /opt/microsoft/powershell/7.0.1
+tar zxf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/7.0.1
+chmod +x /opt/microsoft/powershell/7.0.1/pwsh
+ln -s /opt/microsoft/powershell/7.0.1/pwsh /usr/bin/pwsh
 rm -rf /tmp/powershell.tar.gz
 fi
 
