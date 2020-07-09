598451270f7283dc4c086b7644f766033a453f11
MaynardMiner <maynardviii@gmail.com>
2020-07-08 20:14:40 -0400

    add bminer to updater

--- build/powershell/startup/remoteagent.psm1
+++ build/powershell/startup/remoteagent.psm1
@@ -559,6 +559,11 @@ Access Denied Error prevented.
                                                 $Data.$_.naming | Add-Member "beamv2" "beamv2" -ErrorAction SilentlyContinue
                                                 $Data.$_.fee | Add-Member "beamv2" 2 -ErrorAction SilentlyContinue
 
+                                                $Data.$_.commands | Add-Member "beamhashv3" "" -ErrorAction SilentlyContinue
+                                                $Data.$_.difficulty | Add-Member "beamhashv3" "" -ErrorAction SilentlyContinue 
+                                                $Data.$_.naming | Add-Member "beamhashv3" "beamhashv3" -ErrorAction SilentlyContinue
+                                                $Data.$_.fee | Add-Member "beamhashv3" 2 -ErrorAction SilentlyContinue
+
                                                 $Data.$_.commands = $Data.$_.commands | Select-Object -ExcludeProperty "cuckaroo29d", "cuckaroo29"
                                                 $Data.$_.difficulty = $Data.$_.difficulty | Select-Object -ExcludeProperty "cuckaroo29d", "cuckaroo29"
                                                 $Data.$_.naming = $Data.$_.naming | Select-Object -ExcludeProperty "cuckaroo29d", "cuckaroo29"
