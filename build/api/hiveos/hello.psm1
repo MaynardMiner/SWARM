6f2c2840124de64defb647fd8cca085446c856e5
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2020-08-26 22:12:59 -0400

    new method to parse json from flight sheet

--- build/api/hiveos/hello.psm1
+++ build/api/hiveos/hello.psm1
@@ -129,24 +129,42 @@ function Global:Start-WebStartup($response, $Site) {
                     $global:Config.$Params | ConvertTo-Json | Set-Content ".\config\parameters\$($Params)_keys.json"
                 }
                 "wallet" {
-                    $arguments = [string]$RigConf.result.wallet | ConvertFrom-StringData
-                    if($arguments.CUSTOM_USER_CONFIG) {
-                        ## Remove the "'" at front and end.
-                        $arguments = $arguments.CUSTOM_USER_CONFIG.TrimStart("'").TrimEnd("'");
-
-                    } else {
+                    $parser = [string]$response.result.wallet;
+                    $new = $parser;
+                    $joined = $parser.replace("`n","");
+                    $start_joined = $joined.IndexOf("CUSTOM_USER_CONFIG=`'{");
+                    if($start_joined -ne -1) {
+                        $start = $parser.IndexOf("CUSTOM_USER_CONFIG=");
+                        $end = $parser.Substring($start + 20).IndexOf("`'");
+                        $end_joined = $joined.Substring($start_joined + 20).IndexOf("`'");
+                        $condensed = $joined.Substring(($start_joined + 19),($end_joined + 2));
+                        $new = $parser.remove($start + 19, $end + 2).Insert($start + 19,$condensed);
+                    }
+                    $Wallet = $new | ConvertFrom-StringData
+                    for ($i = 0; $i -lt $Wallet.keys.Count; $i++) {
+                        $key = $Wallet.Keys | Select-Object -Skip $i -First 1;
+                        $Wallet.$key = $Wallet.$key.TrimStart("`"");
+                        $Wallet.$key = $Wallet.$key.TrimEnd("`"");
+                        $Wallet.$key = $Wallet.$key.TrimStart("`'");
+                        $Wallet.$key = $Wallet.$key.TrimEnd("`'");
+                    }        
+                    if(!$Wallet.CUSTOM_USER_CONFIG) {
                         Write-Log "Warning: No CUSTOM_USER_CONFIG found!" -ForegroundColor Red
                         Write-Log "Make sure you are using a Custom User Config section in HiveOS" -ForegroundColor Red
                     }
+                    $arguments = $Wallet.CUSTOM_USER_CONFIG
                     if ($arguments -like "*-wallet1*" -or $arguments -like "*`"wallet1`"*") {
                         $argjson = @{ }
-                        try { $test = "$arguments" | ConvertFrom-Json; if ($test) { $isjson = $true } } catch { $isjson = $false }
+                        $isjson = $false;
+                        try { 
+                            $test = $arguments.CUSTOM_USER_CONFIG | ConvertFrom-Json; 
+                            $isjson = $true;
+                        } catch { }
                         if ($isjson) {
                             $Params = @{ }
                             $test.PSObject.Properties.Name | Foreach-Object { $Params.Add("$($_)", $test.$_) }
                             $Defaults = Get-Content ".\config\parameters\default.json" | ConvertFrom-Json
                             $Defaults.PSObject.Properties.Name | Foreach-Object { if ($_ -notin $Params.keys) { $Params.Add("$($_)", $Defaults.$_) } }
-    
                         }
                         else {
                             $arguments = $arguments -split " -"
