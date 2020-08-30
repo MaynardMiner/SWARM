6e36a2c6da5fd4c5b8622a1edaf935f072dc07c6
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2020-08-29 20:34:16 -0400

    -Hashrate_Reduction Update   Narrow hashrate adjustment to miner combinations   of the same coin only for auto-coin

--- build/powershell/miner/sorting.psm1
+++ build/powershell/miner/sorting.psm1
@@ -436,9 +436,9 @@ function Global:Start-Sorting {
         ## This means that miner must be x % better in hashrate/rej ratio to switch. Where x% is -Hashrate_Threshold.
         ## This does not factor pool- If another pool was more profitable, it was going to switch anyways.
         $IsBestMiner = ($Null -ne (($(vars).BestActiveMiners | Where-Object Path -EQ $Miner.Path | Where-Object Symbol -Eq $Miner.Symbol | Where-Object Arguments -EQ $Miner.Arguments | Where-Object Type -EQ $Miner.Type)))
-        $IsSameAlgoAsBestMiner = ($Null -ne (($(vars)).BestActiveMiners | Where-Object Algo -eq $Miner.Algo | Where-Object Type -eq $Miner.Type))
+        $IsSameSymbolsAsBestMiner = ($Null -ne (($(vars)).BestActiveMiners | Where-Object Symbol -eq $Miner.Symbol | Where-Object Type -eq $Miner.Type))
         
-        if($(arg).Hashrate_Threshold -gt 0 -and !$IsSameAlgoAsBestMiner -and $IsBestMiner) {
+        if($(arg).Hashrate_Threshold -gt 0 -and !$IsSameSymbolsAsBestMiner -and $IsBestMiner) {
             $Miner.Quote = ($Miner.Hashrate_Adjusted * (1 - ( $(arg).Hashrate_Threshold / 100) ) ) * $Quote
         }
 
