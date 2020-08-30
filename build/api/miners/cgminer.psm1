831591f7b0da79ca70fd5e47e2a2f442ff624fe4
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2020-08-30 17:14:08 -0400

    add license

--- build/api/miners/cgminer.psm1
+++ build/api/miners/cgminer.psm1
@@ -1,3 +1,15 @@
+<#
+SWARM is open-source software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+SWARM is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#>
 function Global:Get-StatsCgminer {
     $Hash_Table = @{HS = 1; KHS = 1000; MHS = 1000000; GHS = 1000000000; THS = 1000000000000; PHS = 1000000000000000 }
     $Command = "summary|0"
