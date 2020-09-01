5ce7a75f34c72566d8f8d1f8d02a5f62b6851599
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2020-09-01 11:58:22 -0400

    Fix $Pass issue in Gminer

--- miners/gpu/nvidia/gminer.ps1
+++ miners/gpu/nvidia/gminer.ps1
@@ -83,7 +83,7 @@ $(vars).NVIDIATypes | ForEach-Object {
                 $SelAlgo = $_.Algorithm
                 $SelName = $_.Name
                 if ($MinerConfig.$ConfigType.difficulty.$($_.Algorithm)) { $Diff = ",d=$($MinerConfig.$ConfigType.difficulty.$($_.Algorithm))" }
-                $Pass = " --pass $($_.$Pass)$Diff "
+                $UserPass = " --pass $($_.$Pass)$Diff "
                 $GetUser = $_.$User;
                 $Worker = $_.Worker;
                 switch ($SelAlgo) {
@@ -128,7 +128,7 @@ $(vars).NVIDIATypes | ForEach-Object {
                     Stratum    = "$($_.Protocol)://$($_.Pool_Host):$($_.Port)" 
                     Version    = "$($(vars).nvidia.gminer.version)"
                     DeviceCall = "gminer"
-                    Arguments  = "--api $Port --server $($_.Pool_Host) --nvml 0 --port $($_.Port) $AddArgs--user $GetUser$Pass --logfile `'$Log`' $($MinerConfig.$ConfigType.commands.$($_.Algorithm))"
+                    Arguments  = "--api $Port --server $($_.Pool_Host) --nvml 0 --port $($_.Port) $AddArgs--user $GetUser$UserPass --logfile `'$Log`' $($MinerConfig.$ConfigType.commands.$($_.Algorithm))"
                     HashRates  = $Stat.Hour
                     HashRate_Adjusted = $Hashstat
                     Quote      = $_.Price
