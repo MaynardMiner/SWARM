2c0509b379ee774dee61dac3d0b0c7ad9d1d4ef1
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2020-09-03 03:11:27 -0400

    fix bminer AGAIN

--- miners/gpu/nvidia/bminer.ps1
+++ miners/gpu/nvidia/bminer.ps1
@@ -61,11 +61,12 @@ $(vars).NVIDIATypes | ForEach-Object {
             $Pools | Where-Object Algorithm -eq $MinerAlgo |  ForEach-Object {              
                 $Sel = $_.Algorithm
                 $SelName = $_.Name
-                $Pass = $_.$Pass;
-                if ($_.Worker) { $Pass = "$($_.Worker)" }
+                $GetPass = $_.$Pass;
+                $GetUser = $_.$User;
+                if ($_.Worker) { $GetPass = "$($_.Worker)" }
                 $CanUse = $true;
                 if ($MinerConfig.$ConfigType.difficulty.$($_.Algorithm)) { $Diff = "d=$($MinerConfig.$ConfigType.difficulty.$($_.Algorithm))" }else { $Diff = "" }
-                $UserPass = $_.$User + ":" + [System.Web.HttpUtility]::UrlEncode($Pass + $Diff);
+                $UserPass = $_.$User + ":" + [System.Web.HttpUtility]::UrlEncode($GetPass + $Diff);
                 $PoolPort = $_.Port
                 Switch ($SelName) {
                     "nicehash" {
@@ -73,7 +74,7 @@ $(vars).NVIDIATypes | ForEach-Object {
                             "ethash" { $Naming = "ethstratum"; $AddArgs = " " }
                             "cuckaroom" { $Naming = "cuckaroo29m"; $AddArgs = " " }
                             "cuckaroo29-bfc" { $Naming = "bfc"; $AddArgs = " " }
-                            "cuckatoo31" {$Naming = "cuckatoo31"; $AddArgs = " " }
+                            "cuckatoo31" { $Naming = "cuckatoo31"; $AddArgs = " " }
                             "equihash_150/5" { $Naming = "beam"; $AddArgs = " -pers auto " }
                             "equihash_144/5" { $Naming = "zhash"; $AddArgs = " -pers auto " }
                             "beamv2" { $Naming = "beamhash2"; $AddArgs = " " }
@@ -85,12 +86,12 @@ $(vars).NVIDIATypes | ForEach-Object {
                     }
                     "zergpool" {
                         switch ($Sel) {
-                            "ethash" { $Pass = ""; $Naming = "ethproxy"; $AddArgs = ""; $CanUse = $true; $UserPass = $_.$User + "." + ":" + [System.Web.HttpUtility]::UrlEncode($Pass + $Diff) }
-                            "equihash_144/5" { $Pass = ""; $Naming = "zhash"; $AddArgs = " -pers auto "; $CanUse = $false }
-                            "equihash_150/5" { $Pass = ""; $Naming = "beam"; $AddArgs = " -pers auto " ; $CanUse = $false }
-                            "equihash_144/5" { $Pass = ""; $Naming = "zhash"; $AddArgs = " -pers auto "; $CanUse = $false }
-                            "eaglesong" { $Pass = ""; $Naming = "eaglesong"; $AddArgs = " "; $CanUse = $true; $UserPass = $_.$User + "." + ":" + [System.Web.HttpUtility]::UrlEncode($Pass + $Diff)}
-                            "kawpow" { $Pass = ""; $Naming = "raven"; $AddArgs = " "; $CanUse = $true; $UserPass = $_.$User + "." + ":" + [System.Web.HttpUtility]::UrlEncode($Pass + $Diff) }
+                            "ethash" { $Naming = "ethproxy"; $AddArgs = " "; $UserPass = $GetUser + "." + ":" + [System.Web.HttpUtility]::UrlEncode($GetPass + $Diff) }
+                            "equihash_144/5" { $Naming = "zhash"; $AddArgs = " -pers auto "; $CanUse = $false }
+                            "equihash_150/5" { $Naming = "beam"; $AddArgs = " -pers auto " ; $CanUse = $false }
+                            "equihash_144/5" { $Naming = "zhash"; $AddArgs = " -pers auto "; $CanUse = $false }
+                            "eaglesong" { $Naming = "eaglesong"; $AddArgs = " "; $UserPass = $GetUser + "." + ":" + [System.Web.HttpUtility]::UrlEncode($GetPass + $Diff) }
+                            "kawpow" { $Naming = "raven"; $AddArgs = " "; $UserPass = $GetUser + "." + ":" + [System.Web.HttpUtility]::UrlEncode($GetPass + $Diff) }
                         }
                     }
                     "whalesburg" {
@@ -104,35 +105,37 @@ $(vars).NVIDIATypes | ForEach-Object {
                         }
                     }
                 }
-                [PSCustomObject]@{
-                    MName      = $Name
-                    Coin       = $(vars).Coins
-                    Delay      = $MinerConfig.$ConfigType.delay
-                    Fees       = $MinerConfig.$ConfigType.fee.$($_.Algorithm)
-                    Symbol     = "$($_.Symbol)"
-                    MinerName  = $MinerName
-                    Prestart   = $PreStart
-                    Type       = $ConfigType
-                    Path       = $Path
-                    Devices    = $Devices
-                    Stratum    = "$($_.Protocol)://$($_.Pool_Host):$($_.Port)" 
-                    Version    = "$($(vars).nvidia.bminer.version)"
-                    DeviceCall = "bminer"
-                    Arguments  = "-uri $($Naming)://$UserPass@$($_.Pool_Host):$($PoolPort) $AddArgs-logfile `'$Log`' -api 127.0.0.1:$Port $($MinerConfig.$ConfigType.commands.$($_.Algorithm))"
-                    HashRates  = $Stat.Hour
-                    HashRate_Adjusted = $Hashstat
-                    Quote      = $_.Price
-                    Rejections = $Stat.Rejections
-                    Power      = if ($(vars).Watts.$($_.Algorithm)."$($ConfigType)_Watts") { $(vars).Watts.$($_.Algorithm)."$($ConfigType)_Watts" }elseif ($(vars).Watts.default."$($ConfigType)_Watts") { $(vars).Watts.default."$($ConfigType)_Watts" }else { 0 } 
-                    MinerPool  = "$($_.Name)"
-                    Port       = $Port
-                    Worker     = $Rig
-                    API        = "bminer"
-                    Wallet     = "$($_.$User)"
-                    URI        = $Uri
-                    Server     = "localhost"                        
-                    Algo       = "$($_.Algorithm)"
-                    Log        = "miner_generated"
+                if ($CanUse) {
+                    [PSCustomObject]@{
+                        MName             = $Name
+                        Coin              = $(vars).Coins
+                        Delay             = $MinerConfig.$ConfigType.delay
+                        Fees              = $MinerConfig.$ConfigType.fee.$($_.Algorithm)
+                        Symbol            = "$($_.Symbol)"
+                        MinerName         = $MinerName
+                        Prestart          = $PreStart
+                        Type              = $ConfigType
+                        Path              = $Path
+                        Devices           = $Devices
+                        Stratum           = "$($_.Protocol)://$($_.Pool_Host):$($_.Port)" 
+                        Version           = "$($(vars).nvidia.bminer.version)"
+                        DeviceCall        = "bminer"
+                        Arguments         = "-uri $($Naming)://$UserPass@$($_.Pool_Host):$($PoolPort) $AddArgs-logfile `'$Log`' -api 127.0.0.1:$Port $($MinerConfig.$ConfigType.commands.$($_.Algorithm))"
+                        HashRates         = $Stat.Hour
+                        HashRate_Adjusted = $Hashstat
+                        Quote             = $_.Price
+                        Rejections        = $Stat.Rejections
+                        Power             = if ($(vars).Watts.$($_.Algorithm)."$($ConfigType)_Watts") { $(vars).Watts.$($_.Algorithm)."$($ConfigType)_Watts" }elseif ($(vars).Watts.default."$($ConfigType)_Watts") { $(vars).Watts.default."$($ConfigType)_Watts" }else { 0 } 
+                        MinerPool         = "$($_.Name)"
+                        Port              = $Port
+                        Worker            = $Rig
+                        API               = "bminer"
+                        Wallet            = "$($_.$User)"
+                        URI               = $Uri
+                        Server            = "localhost"                        
+                        Algo              = "$($_.Algorithm)"
+                        Log               = "miner_generated"
+                    }
                 }
             }
         }
