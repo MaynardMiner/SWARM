f787f22f0e8ee9a563c447968b38e1e79963ba7e
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2020-09-03 11:36:34 -0400

    Miscommunication: User relayed ethproxy not ethstratum.2.0. Was referring to tt-miner. Revert bminer last change, and change tt-miner

--- miners/gpu/nvidia/tt-miner.ps1
+++ miners/gpu/nvidia/tt-miner.ps1
@@ -85,7 +85,7 @@ $(vars).NVIDIATypes | ForEach-Object {
                     Stratum    = "$($_.Protocol)://$($_.Pool_Host):$($_.Port)" 
                     Version    = "$($(vars).nvidia.$CName.version)"
                     DeviceCall = "ttminer"
-                    Arguments  = "-a $($MinerConfig.$ConfigType.naming.$($_.Algorithm)) --nvidia -o $($_.Protocol)://$($_.Pool_Host):$($_.Port) $Worker-b localhost:$Port -u $($_.$User) -p $($_.$Pass) -log `'$Log`' $($MinerConfig.$ConfigType.commands.$($_.Algorithm))"
+                    Arguments  = "-a $($MinerConfig.$ConfigType.naming.$($_.Algorithm)) --nvidia -o $($_.Pool_Host):$($_.Port) $Worker-b localhost:$Port -u $($_.$User) -p $($_.$Pass) -log `'$Log`' $($MinerConfig.$ConfigType.commands.$($_.Algorithm))"
                     HashRates  = $Stat.Hour
                     HashRate_Adjusted = $Hashstat
                     Quote      = $_.Price
