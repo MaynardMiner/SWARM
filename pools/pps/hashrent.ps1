6bd5ccea96c6f4cde8675365d90b632174810891
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2020-09-18 14:25:39 -0400

    test pool

---
+++ pools/pps/hashrent.ps1
@@ -0,0 +1,81 @@
+<#
+SWARM is open-source software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+SWARM is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#>
+
+. .\build\powershell\global\modules.ps1
+. .\build\powershell\global\classes.ps1
+
+if ($Name -in $(arg).PoolName) {
+
+    $Request = [PSCustomObject]@{ } 
+
+    try { $request = Invoke-Webrequest "https://hashrent.pro/" -TimeoutSec 10 -ErrorAction Stop } 
+    catch { return "WARNING: SWARM contacted ($Name) but there was no response." }
+ 
+    if (!$request.content) {
+        return "WARNING: SWARM contacted ($Name) but ($Name) the response was empty."        
+    } 
+    
+    $Hashrent_Port = "8008"
+    Switch ($(arg).Location) {
+        "US" { $Region = "eu"; $Hashrent_Port = "62054" }
+        "ASIA" { $Region = "ru" }
+        "EUROPE" { $Region = "eu"; $Hashrent_Port = "62054" }
+        "JAPAN" { $Region = "ru" }
+    }
+
+    if ($(vars).Algorithm -contains "ethash") {
+        $Hashrent_Host = $Region + ".hashrent.pro";
+        $Fee = 0.5;
+        $GH = [convert]::ToDecimal([regex]::match($request.content, '<span class="GH_Revenue">([^/)]+?)</span>').Value.Replace('<span class="GH_Revenue">', '').Replace('</span>', ''));
+        $Estimate = [Convert]::ToDecimal(($GH / 1000000000));    
+        $Value = [convert]::ToDecimal($Estimate * (1 - ($Fee / 100)));
+        $Stat = [Pool_Stat]::New("$($Name)_$($Whalesburg_Algorithm)", $Value, 0, -1, $false)
+        $Level = $Stat.$($(arg).Stat_Algo)
+        $previous = $Stat.Day_MA
+
+        $User1 = $(arg).SuperWallet + ".$($(arg).WorkerInstance)"
+        ## User2
+        $User2 = $(arg).SuperWallet + ".$($(arg).WorkerInstance)"
+        ## User3
+        $User3 = $(arg).SuperWallet + ".$($(arg).WorkerInstance)"
+
+        [Pool]::New(
+            ## Symbol
+            "Ethash-Algo",
+            ## Algorithm
+            "Ethash",
+            ## Level
+            $Level,
+            ## Stratum
+            "stratum+tcp",
+            ## Pool_Host
+            $Hashrent_Host,
+            ## Pool_Port
+            $Hashrent_Port,
+            ## User1
+            $User1,
+            ## User2
+            $User2,
+            ## User3
+            $User3,
+            ## Pass1
+            "x",
+            ## Pass2
+            "x",
+            ## Pass3
+            "x",
+            ## Previous
+            $previous
+        )
+    }
+}
