f1b969b75c9e9d48f2291deb36ae05fa27ab5183
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2020-10-05 13:27:56 -0400

    using clinfo to get amd platform.  -Set "0" if it fails.

--- build/powershell/startup/cl.psm1
+++ build/powershell/startup/cl.psm1
@@ -12,22 +12,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #>
 
 function Global:get-AMDPlatform {
-
-    if ($(arg).Platform -eq "linux") {
-        $A = Invoke-Expression ".\build\apps\platforms\getplatforms" | Tee-Object -Variable amdclplatform
-        Start-Sleep -S .5
-        $GPUPlatform = $amdclplatform | Select-String "AMD Accelerated Parallel Processing"
-        $GPUPlatform = $GPUPlatform -replace (" ", "")
-        $GPUPlatform = $GPUPlatform -split "AMD" | Select-Object -First 1
-        $GPUPlatform
-    }
-
-    if ($(arg).Platform -eq "windows") {
+        $Platform = "0"
         $A = (clinfo) | Select-string "Platform Vendor"
         $PlatformA = @()
         for ($i = 0; $i -lt $A.Count; $i++) { $PlatSel = $A | Select-Object -Skip $i -First 1; $PlatSel = $PlatSel -replace "Platform Vendor", "$i"; $PlatSel = $PlatSel -replace ":", "="; $PlatformA += $PlatSel}
         $PlatformA = $PlatformA | ConvertFrom-StringData
-        $PlatformA.keys | ForEach-Object {if ($PlatformA.$_ -eq "AMD Accelerated Parallel Processing" -or $PlatformA.$_ -eq "Advanced Micro Devices, Inc.") {$B = $_}}
-        $B
-    }
+        $PlatformA.keys | ForEach-Object {if ($PlatformA.$_ -eq "AMD Accelerated Parallel Processing" -or $PlatformA.$_ -eq "Advanced Micro Devices, Inc.") {$Platform = $_}}
+        return $Platform
 }
