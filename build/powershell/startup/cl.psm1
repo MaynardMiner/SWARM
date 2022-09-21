d1959b4e521cc1804849cfd56600b452def4a3a2
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2022-09-20 21:05:57 -0400

    fix

--- build/powershell/startup/cl.psm1
+++ build/powershell/startup/cl.psm1
@@ -11,12 +11,23 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #>
 
-function Global:get-AMDPlatform {
-        $Platform = "0"
+function Global:Get-AMDPlatform() {
+    $Opencl = 0;
+    $A = ($(clinfo) | Select-String "Platform Vendor").Line;
+    if($IsLinux){ 
+        $A.Replace("  Platform Vendor                                 ","")
+        foreach($platform in $A) {
+            if($platform -eq "AMD Accelerated Parallel Processing" -or $platform -eq "Advanced Micro Devices, Inc.") {
+                $Opencl = $i;
+            }
+        }
+    }
+    if($IsWindows) {
         $A = (clinfo) | Select-string "Platform Vendor"
         $PlatformA = @()
         for ($i = 0; $i -lt $A.Count; $i++) { $PlatSel = $A | Select-Object -Skip $i -First 1; $PlatSel = $PlatSel -replace "Platform Vendor", "$i"; $PlatSel = $PlatSel -replace ":", "="; $PlatformA += $PlatSel}
         $PlatformA = $PlatformA | ConvertFrom-StringData
-        $PlatformA.keys | ForEach-Object {if ($PlatformA.$_ -eq "AMD Accelerated Parallel Processing" -or $PlatformA.$_ -eq "Advanced Micro Devices, Inc.") {$Platform = $_}}
-        return $Platform
+        $PlatformA.keys | ForEach-Object {if ($PlatformA.$_ -eq "AMD Accelerated Parallel Processing" -or $PlatformA.$_ -eq "Advanced Micro Devices, Inc.") {$Opencl = $_}}
+    }
+    return $Opencl
 }
