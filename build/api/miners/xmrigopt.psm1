c6323495c48791f930be1d73ca1ef1a737528cb2
MaynardMiner <38481606+MaynardMiner@users.noreply.github.com>
2022-09-23 18:47:18 -0400

    xmrig cpu

--- build/api/miners/xmrigopt.psm1
+++ build/api/miners/xmrigopt.psm1
@@ -16,31 +16,19 @@ function Global:Get-Statsxmrigopt {
     $Request = Global:Get-HTTP -Port $global:Port -Message $Message
     if ($Request) {
         try { $Data = $Request.Content | ConvertFrom-Json -ErrorAction Stop; }catch { Write-Host "Failed To gather summary" -ForegroundColor Red; break }
-        $HashRate_Total = [Double]$Data.hashrate.total[0]
-        if (-not $HashRate_Total) { $HashRate_Total = [Double]$Data.hashrate.total[1] } #fix
-        if (-not $HashRate_Total) { $HashRate_Total = [Double]$Data.hashrate.total[2] } #fix
+        foreach ($hash in $Data.hashrate.total) {
+            $GetHash = 0;
+            $IsInt = [Double]::TryParse($hash, $GetHash);
+            if ($IsInt) {
+                break;
+            }
+        }
+        $HashRate_Total = $GetHash;
         $global:RAW = $HashRate_Total
         $global:CPUKHS = $HashRate_Total / 1000
         Global:Write-MinerData2
         Write-Host "Note: XMR-STAK/XMRig API is not great. You can't match threads to specific CPU." -ForegroundColor Yellow
         $Hash = @()
-        try { 
-            for ($global:i = 0; $global:i -lt $Data.hashrate.threads.count; $global:i++) {
-                $Hash += 
-                ## If there is more than one count
-                if ($Data.hashrate.threads[$global:i].count -gt 1) {
-                    $Data.Hashrate.threads[$global:i] | Select-Object -First 1 
-                }
-                ## If not select that singular item.
-                else { $Data.Hashrate.threads[$Global:i] }
-            }
-        }
-        catch { }
-        try { 
-            for ($global:i = 0; $global:i -lt $Devices.Count; $global:i++) { 
-                $global:CPUHashrates.$global:i = ($Hash[$global:i]) / 1000 
-            } 
-        }
         catch { Write-Host "Failed To parse threads" -ForegroundColor Red };
         $global:MinerACC += $Data.results.shares_good
         $global:MinerREJ += [Double]$Data.results.shares_total - [Double]$Data.results.shares_good
